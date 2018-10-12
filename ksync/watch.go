47b55791367920b48c99d9ee715ee0ce700c0413
Ramiro Berrelleza <rberrelleza@gmail.com>
2018-10-12 22:47:31 +0200

    start ksync as a daemon

--- ksync/watch.go
+++ ksync/watch.go
@@ -6,6 +6,6 @@ import (
 
 //Watch watches for changes in the local folder
 func Watch() error {
-	cmd := exec.Command("ksync", "watch")
+	cmd := exec.Command("ksync", "watch", "--daemon")
 	return cmd.Start()
 }
