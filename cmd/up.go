95adc7c8f001b45820e0bb65314b3cf30d9907ee
Ramiro Berrelleza <rberrelleza@gmail.com>
2019-02-05 15:35:32 -0800

    make UpContext's properties public

--- cmd/up.go
+++ cmd/up.go
@@ -32,19 +32,19 @@ import (
 type UpContext struct {
 	Context        context.Context
 	Cancel         context.CancelFunc
-	wg             *sync.WaitGroup
-	dev            *model.Dev
-	client         *kubernetes.Clientset
-	restConfig     *rest.Config
-	currentContext string
-	namespace      string
-	deployment     *appsv1.Deployment
-	deploymentName string
-	pod            *apiv1.Pod
-	forwarder      *forward.CNDPortForwardManager
-	disconnect     chan struct{}
-	reconnect      chan struct{}
-	sy             *syncthing.Syncthing
+	WG             *sync.WaitGroup
+	Dev            *model.Dev
+	Client         *kubernetes.Clientset
+	RestConfig     *rest.Config
+	CurrentContext string
+	Namespace      string
+	Deployment     *appsv1.Deployment
+	DeploymentName string
+	Pod            *apiv1.Pod
+	Forwarder      *forward.CNDPortForwardManager
+	Disconnect     chan struct{}
+	Reconnect      chan struct{}
+	Sy             *syncthing.Syncthing
 }
 
 //Up starts a cloud native environment
@@ -58,7 +58,7 @@ func Up() *cobra.Command {
 			log.Debugf("starting up command")
 			up := &UpContext{}
 			var err error
-			up.dev, err = model.ReadDev(devPath)
+			up.Dev, err = model.ReadDev(devPath)
 			if err != nil {
 				return err
 			}
@@ -67,13 +67,13 @@ func Up() *cobra.Command {
 			defer analytics.Send(analytics.EventUpEnd, GetActionID())
 			fmt.Println("Activating your cloud native development environment...")
 
-			up.namespace = namespace
-			up.wg = &sync.WaitGroup{}
+			up.Namespace = namespace
+			up.WG = &sync.WaitGroup{}
 			up.Context, up.Cancel = context.WithCancel(context.Background())
 			defer up.Shutdown()
 
-			up.disconnect = make(chan struct{}, 1)
-			up.reconnect = make(chan struct{}, 1)
+			up.Disconnect = make(chan struct{}, 1)
+			up.Reconnect = make(chan struct{}, 1)
 
 			err = up.Execute()
 			if err != nil {
@@ -82,11 +82,11 @@ func Up() *cobra.Command {
 
 			up.StreamLogsAndEvents()
 
-			fmt.Printf("Linking '%s' to %s/%s...", up.dev.Mount.Source, up.deploymentName, up.dev.Swap.Deployment.Container)
+			fmt.Printf("Linking '%s' to %s/%s...", up.Dev.Mount.Source, up.DeploymentName, up.Dev.Swap.Deployment.Container)
 			fmt.Println()
 			log.Green("Ready. Go to your IDE and start coding 😎!")
 
-			log.Debugf("%s ready, waiting for stop signal to shut down", up.deploymentName)
+			log.Debugf("%s ready, waiting for stop signal to shut down", up.DeploymentName)
 
 			return up.WaitUntilExit()
 		},
@@ -115,14 +115,14 @@ func (up *UpContext) WaitUntilExit() error {
 			log.Debugf("CTRL+C received, starting shutdown sequence")
 			fmt.Println()
 			return nil
-		case <-up.reconnect:
+		case <-up.Reconnect:
 			reconnectionAttempts = 0
 			if displayReconnectionNotification {
 				log.Green("Reconnected to your cluster.")
 				displayDisconnectionNotification = true
 				displayReconnectionNotification = false
 			}
-		case <-up.disconnect:
+		case <-up.Disconnect:
 			log.Infof("cluster connection lost, reconnecting %d/%d", reconnectionAttempts, maxReconnectionAttempts)
 			reconnectionAttempts++
 			if reconnectionAttempts > maxReconnectionAttempts {
@@ -138,7 +138,7 @@ func (up *UpContext) WaitUntilExit() error {
 			if err := up.ReconnectPortForward(); err != nil {
 				log.Infof("failed to reconnect port forward. will retry: %s", err)
 			}
-		case err := <-up.forwarder.ErrChan:
+		case err := <-up.Forwarder.ErrChan:
 			log.Yellow(err.Error())
 		case <-resetAttempts.C:
 			log.Debug("resetting reconnection attempts counter")
@@ -163,80 +163,80 @@ func (up *UpContext) Execute() error {
 		return fmt.Errorf("there is already an entry for %s/%s Are you running '%s up' somewhere else?", config.GetBinaryName(), deployments.GetFullName(n, deploymentName), c)
 	}
 
-	up.namespace, up.client, up.restConfig, up.currentContext, err = k8Client.Get(up.namespace)
+	up.Namespace, up.Client, up.RestConfig, up.CurrentContext, err = k8Client.Get(up.Namespace)
 	if err != nil {
 		return err
 	}
 
-	up.deploymentName = deployments.GetFullName(up.namespace, up.dev.Swap.Deployment.Name)
-	log.Debugf("getting deployment %s", up.deploymentName)
+	up.DeploymentName = deployments.GetFullName(up.Namespace, up.Dev.Swap.Deployment.Name)
+	log.Debugf("getting deployment %s", up.DeploymentName)
 
-	up.deployment, err = deployments.Get(up.namespace, up.dev.Swap.Deployment.Name, up.client)
+	up.Deployment, err = deployments.Get(up.Namespace, up.Dev.Swap.Deployment.Name, up.Client)
 	if err != nil {
 		log.Debug(err)
 		if strings.Contains(err.Error(), "not found") {
-			return fmt.Errorf("deployment %s not found [current context: %s]", up.deploymentName, up.currentContext)
+			return fmt.Errorf("deployment %s not found [current context: %s]", up.DeploymentName, up.CurrentContext)
 		}
 
-		return fmt.Errorf("couldn't get deployment %s from your cluster. Please try again", up.deploymentName)
+		return fmt.Errorf("couldn't get deployment %s from your cluster. Please try again", up.DeploymentName)
 	}
 
-	up.dev.Swap.Deployment.Container = deployments.GetDevContainerOrFirst(
-		up.dev.Swap.Deployment.Container,
-		up.deployment.Spec.Template.Spec.Containers,
+	up.Dev.Swap.Deployment.Container = deployments.GetDevContainerOrFirst(
+		up.Dev.Swap.Deployment.Container,
+		up.Deployment.Spec.Template.Spec.Containers,
 	)
 
-	devList, err := deployments.GetAndUpdateDevListFromAnnotation(up.deployment.GetObjectMeta(), up.dev)
+	devList, err := deployments.GetAndUpdateDevListFromAnnotation(up.Deployment.GetObjectMeta(), up.Dev)
 	if err != nil {
 		return err
 	}
 
-	up.sy, err = syncthing.NewSyncthing(up.namespace, up.deployment.Name, devList)
+	up.Sy, err = syncthing.NewSyncthing(up.Namespace, up.Deployment.Name, devList)
 	if err != nil {
 		return err
 	}
 
-	log.Debugf("enabling dev mode on %s", up.deploymentName)
-	if err := deployments.DevModeOn(up.deployment, devList, up.client); err != nil {
+	log.Debugf("enabling dev mode on %s", up.DeploymentName)
+	if err := deployments.DevModeOn(up.Deployment, devList, up.Client); err != nil {
 		return err
 	}
 
-	log.Debugf("enabled dev mode on %s", up.deploymentName)
+	log.Debugf("enabled dev mode on %s", up.DeploymentName)
 
-	up.pod, err = deployments.GetCNDPod(up.Context, up.deployment, up.client)
+	up.Pod, err = deployments.GetCNDPod(up.Context, up.Deployment, up.Client)
 	if err != nil {
 		return err
 	}
 
-	if err := deployments.WaitForDevPodToBeRunning(up.Context, up.client, up.namespace, up.pod.Name); err != nil {
+	if err := deployments.WaitForDevPodToBeRunning(up.Context, up.Client, up.Namespace, up.Pod.Name); err != nil {
 		return err
 	}
 
-	if err := up.sy.Run(up.Context, up.wg); err != nil {
+	if err := up.Sy.Run(up.Context, up.WG); err != nil {
 		return err
 	}
 
-	err = storage.Insert(up.Context, up.wg, up.namespace, up.dev, up.sy.GUIAddress)
+	err = storage.Insert(up.Context, up.WG, up.Namespace, up.Dev, up.Sy.GUIAddress)
 	if err != nil {
 		if err == storage.ErrAlreadyRunning {
-			log.Infof("failed to insert new state value for %s", up.deploymentName)
-			return fmt.Errorf("there is already an entry for %s. Are you running '%s up' somewhere else?", config.GetBinaryName(), up.deploymentName)
+			log.Infof("failed to insert new state value for %s", up.DeploymentName)
+			return fmt.Errorf("there is already an entry for %s. Are you running '%s up' somewhere else?", config.GetBinaryName(), up.DeploymentName)
 		}
 		return err
 	}
 
-	up.forwarder = forward.NewCNDPortForwardManager(up.Context, up.restConfig, up.client)
-	if err := up.forwarder.Add(up.sy.RemotePort, syncthing.ClusterPort, up.sy.IsConnected); err != nil {
+	up.Forwarder = forward.NewCNDPortForwardManager(up.Context, up.RestConfig, up.Client)
+	if err := up.Forwarder.Add(up.Sy.RemotePort, syncthing.ClusterPort, up.Sy.IsConnected); err != nil {
 		return err
 	}
 
-	for _, f := range up.dev.Forward {
-		if err := up.forwarder.Add(f.Local, f.Remote, up.sy.IsConnected); err != nil {
+	for _, f := range up.Dev.Forward {
+		if err := up.Forwarder.Add(f.Local, f.Remote, up.Sy.IsConnected); err != nil {
 			return err
 		}
 	}
 
-	up.forwarder.Start(up.pod)
+	up.Forwarder.Start(up.Pod)
 	return nil
 }
 
@@ -244,21 +244,21 @@ func (up *UpContext) Execute() error {
 // - get pod events
 // - get container logs
 func (up *UpContext) StreamLogsAndEvents() {
-	go deployments.GetPodEvents(up.Context, up.pod, up.client)
-	go logs.StreamLogs(up.Context, up.wg, up.deployment, up.dev.Swap.Deployment.Container, up.client)
+	go deployments.GetPodEvents(up.Context, up.Pod, up.Client)
+	go logs.StreamLogs(up.Context, up.WG, up.Deployment, up.Dev.Swap.Deployment.Container, up.Client)
 }
 
 // ReconnectPortForward stops pf and starts it again with the same ports
 func (up *UpContext) ReconnectPortForward() error {
 
-	up.forwarder.Stop()
+	up.Forwarder.Stop()
 
-	pod, err := deployments.GetCNDPod(up.Context, up.deployment, up.client)
+	pod, err := deployments.GetCNDPod(up.Context, up.Deployment, up.Client)
 	if err != nil {
 		return err
 	}
 
-	up.forwarder.Start(pod)
+	up.Forwarder.Start(pod)
 	return nil
 }
 
@@ -270,12 +270,12 @@ func (up *UpContext) Shutdown() {
 	log.Debugf("waiting for tasks for be done")
 	done := make(chan struct{})
 	go func() {
-		up.wg.Wait()
+		up.WG.Wait()
 		close(done)
 	}()
 
 	go func() {
-		up.forwarder.Stop()
+		up.Forwarder.Stop()
 	}()
 
 	select {
