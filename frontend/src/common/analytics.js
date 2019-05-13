ee9fbc28009568b7c340c736cb31d364d6c64f6f
Ramón Lamana <rlamana@gmail.com>
2019-05-13 15:59:52 +0200

    Fix mixpanel dups due to non-production environments user ids (#152)
    * Opt out of mixpanel tracking for non-prod envs
    
    * Enable mixpanel debug mode in dev

--- frontend/src/common/analytics.js
+++ frontend/src/common/analytics.js
@@ -1,13 +1,19 @@
 import mixpanel from 'mixpanel-browser';
 import ga from 'universal-ga';
 
+import environment from './environment';
+
 const GA_TOKEN = 'UA-120828135-3';
 const MIXPANEL_TOKEN = '92fe782cdffa212d8f03861fbf1ea301';
 
 export default {
   init: (user) => {
-    ga.initialize(GA_TOKEN);
-    mixpanel.init(MIXPANEL_TOKEN);
+    ga.initialize(GA_TOKEN, { debug: environment.mode !== 'production' });
+    mixpanel.init(MIXPANEL_TOKEN, {
+      track_pageview: true,
+      debug: environment.mode !== 'production'
+    });
+
     mixpanel.identify(user.id);
     mixpanel.people.set({
       '$name': user.name,
@@ -15,6 +21,12 @@ export default {
       'oktetoId': user.id,
       'githubId': user.githubID
     })
+
+    if (environment.mode !== 'production') {
+      mixpanel.opt_out_tracking({
+        delete_user: true
+      });
+    }
   },
 
   track: (eventName, properties = {}) => {
@@ -31,5 +43,10 @@ export default {
 
   increment: (property, value = 1) => {
     mixpanel.people.increment(property, value);
+  },
+
+  pageview: page => {
+    ga.pageview(page);
+    mixpanel.track('Go to Section', { section: page });
   }
 };
