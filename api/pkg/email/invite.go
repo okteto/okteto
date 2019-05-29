9836b0a2105888bec49fb4615791141a55a5e05b
Ramiro Berrelleza <rberrelleza@gmail.com>
2019-05-29 14:13:33 -0700

    fix invite email copy (#196)

--- api/pkg/email/invite.go
+++ api/pkg/email/invite.go
@@ -1,7 +1,7 @@
 package email
 
 const inviteTextTmpl = `
-{{ .User }} has shared his Okteto space with you.
+{{ .User }} has shared an Okteto space with you.
 
 Okteto helps you accelerate the development of cloud native applications on Kubernetes and collaborate with your teammates. Join {{ .User }} to start developing directly in the cloud!
 
