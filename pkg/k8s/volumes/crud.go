4d3a3cfd780a354f2b7d35cfa3b18ca249a0b4a5
Ramiro Berrelleza <rberrelleza@gmail.com>
2019-08-16 13:46:01 -0700

    block pvc deletion if it's attached

--- pkg/k8s/volumes/crud.go
+++ pkg/k8s/volumes/crud.go
@@ -13,18 +13,57 @@ import (
 	"k8s.io/client-go/kubernetes"
 )
 
+const maxRetries = 300
+
 //Destroy destroys the volume claim for a given dev environment
 func Destroy(name string, dev *model.Dev, c *kubernetes.Clientset) error {
 	vClient := c.CoreV1().PersistentVolumeClaims(dev.Namespace)
 	log.Infof("destroying volume claim '%s'...", name)
-	for {
+
+	if err := checkIfAttached(name, dev, c); err != nil {
+		return err
+	}
+
+	ticker := time.NewTicker(1 * time.Second)
+	for i := 0; i < maxRetries; i++ {
 		err := vClient.Delete(name, &metav1.DeleteOptions{})
 		if err != nil {
 			if strings.Contains(err.Error(), "not found") {
+				log.Infof("volume claim '%s' successfully deleted", name)
 				return nil
 			}
+
 			return fmt.Errorf("error getting kubernetes volume claim: %s", err)
 		}
-		time.Sleep(1 * time.Second)
+
+		<-ticker.C
 	}
+
+	return fmt.Errorf("volume claim '%s' wasn't deleted after 300s", name)
+}
+
+func checkIfAttached(pvc string, dev *model.Dev, c *kubernetes.Clientset) error {
+	pods, err := c.CoreV1().Pods(dev.Namespace).List(metav1.ListOptions{})
+	if err != nil {
+		log.Infof("failed to get available pods: %s", err)
+		return nil
+	}
+
+	for _, p := range pods.Items {
+		log.Infof("checking pod %+v", p)
+		if strings.HasPrefix(p.Name, dev.Name) {
+			continue
+		}
+
+		for _, v := range p.Spec.Volumes {
+			if v.PersistentVolumeClaim != nil {
+				if v.PersistentVolumeClaim.ClaimName == pvc {
+					return fmt.Errorf("can't delete volume claim '%s' since it's still attached to 'pod/%s'", pvc, p.Name)
+				}
+			}
+		}
+	}
+
+	return nil
+
 }
