49cd607c474dced024f67b6e644db9ce4dc02a84
Ramiro Berrelleza <rberrelleza@gmail.com>
2019-09-06 15:03:29 -0700

    show issue suggestion as a hint

--- pkg/errors/errors.go
+++ pkg/errors/errors.go
@@ -40,6 +40,9 @@ var (
 
 	// ErrQuota is returned when there aren't enough resources to enable dev mode
 	ErrQuota = fmt.Errorf("Quota exceeded, please free some resources and try again")
+
+	// ErrSyncFrozen is returned when syncthing has been frozen on the same bytes for 30 seconds
+	ErrSyncFrozen = fmt.Errorf("The synchronization service hasn't made any progress in the last 30 seconds")
 )
 
 // IsNotFound returns true if err is of the type not found
