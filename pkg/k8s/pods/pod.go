3deaae5e511e940ffb858fc0ace6257a65777776
Ramiro Berrelleza <rberrelleza@gmail.com>
2019-09-30 17:07:25 -0700

    wait on restart command (#432)
    Wait for the pods to be ready before exiting the command

--- pkg/k8s/pods/pod.go
+++ pkg/k8s/pods/pod.go
@@ -167,16 +167,94 @@ func Restart(dev *model.Dev, c *kubernetes.Clientset) error {
 	}
 
 	for _, pod := range pods.Items {
-		log.Information("Restarting pod '%s'", pod.Name)
 		err := c.CoreV1().Pods(dev.Namespace).Delete(pod.Name, &metav1.DeleteOptions{GracePeriodSeconds: &devTerminationGracePeriodSeconds})
 		if err != nil {
 			if strings.Contains(err.Error(), "not found") {
-				log.Infof("pod '%s' was already deleted.", pod.Name)
 				return nil
 			}
 			return fmt.Errorf("error deleting kubernetes service: %s", err)
 		}
 	}
 
-	return nil
+	return waitUntilRunning(dev.Namespace, fmt.Sprintf("%s=%s", OktetoDetachedDevLabel, dev.Name), c)
+}
+
+func waitUntilRunning(namespace, selector string, c *kubernetes.Clientset) error {
+	t := time.NewTicker(1 * time.Second)
+	notready := map[string]bool{}
+
+	for i := 0; i < 60; i++ {
+		if i%5 == 0 {
+			log.Infof("checking if pods are ready")
+		}
+
+		pods, err := c.CoreV1().Pods(namespace).List(
+			metav1.ListOptions{
+				LabelSelector: selector,
+			},
+		)
+
+		if err != nil {
+			log.Infof("error listing pods to check status after restart: %s", err)
+			return fmt.Errorf("failed to retrieve dev environment information")
+		}
+
+		allRunning := true
+		for _, pod := range pods.Items {
+			switch pod.Status.Phase {
+			case apiv1.PodPending:
+				allRunning = false
+				notready[pod.GetName()] = true
+			case apiv1.PodFailed:
+				return fmt.Errorf("Pod %s failed to start", pod.Name)
+			case apiv1.PodRunning:
+				if isRunning(&pod) {
+					if _, ok := notready[pod.GetName()]; ok {
+						log.Infof("pod/%s is ready", pod.GetName())
+						delete(notready, pod.GetName())
+					}
+				} else {
+					allRunning = false
+					notready[pod.GetName()] = true
+					if i%5 == 0 {
+						log.Infof("pod/%s is not ready", pod.GetName())
+					}
+				}
+			}
+		}
+
+		if allRunning {
+			log.Infof("pods are ready")
+			return nil
+		}
+
+		<-t.C
+	}
+
+	pods := make([]string, 0, len(notready))
+	for k := range notready {
+		pods = append(pods, k)
+	}
+
+	return fmt.Errorf("Pod(s) %s didn't restart after 60 seconds", strings.Join(pods, ","))
+}
+
+func isRunning(p *apiv1.Pod) bool {
+	if p.Status.Phase != apiv1.PodRunning {
+		return false
+	}
+
+	if p.GetObjectMeta().GetDeletionTimestamp() != nil {
+		return false
+	}
+
+	for _, c := range p.Status.Conditions {
+		if c.Type == apiv1.PodReady {
+			if c.Status == apiv1.ConditionTrue {
+				return true
+			}
+		}
+	}
+
+	return false
 }
