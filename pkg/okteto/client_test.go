c51cc3b1a157585fc26e16feb14b4a64a2078f95
Ramiro Berrelleza <rberrelleza@gmail.com>
2019-10-02 17:22:37 -0700

    test duplicated contexts

--- pkg/okteto/client_test.go
+++ pkg/okteto/client_test.go
@@ -57,4 +57,23 @@ func TestSetKubeConfig(t *testing.T) {
 		t.Errorf("current context was not sf-okteto-com-ns-2, it was %s", cfg.CurrentContext)
 	}
 
+	// add duplicated
+
+	if err := SetKubeConfig(c, file.Name(), "ns-2", "123-123-124", "sf-okteto-com"); err != nil {
+		t.Fatal(err.Error())
+	}
+
+	if err := SetKubeConfig(c, file.Name(), "ns-2", "123-123-123", "cloud-okteto-com"); err != nil {
+		t.Fatal(err.Error())
+	}
+
+	cfg, err = clientcmd.LoadFromFile(file.Name())
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	if len(cfg.Contexts) != 5 {
+		t.Errorf("the config file didn't have five contexts after adding the same twice: %+v", cfg.Contexts)
+	}
+
 }
