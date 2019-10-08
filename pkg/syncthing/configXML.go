57b949cc884403a004d45924a49e668ccafa6996
Ramiro Berrelleza <rberrelleza@gmail.com>
2019-10-08 09:49:30 -0700

    revert listenAddress change

--- pkg/syncthing/configXML.go
+++ pkg/syncthing/configXML.go
@@ -45,7 +45,7 @@ const configXML = `<configuration version="29">
 </gui>
 <ldap></ldap>
 <options>
-    <listenAddress>tcp://{{.ListenAddress}}</listenAddress>
+    <listenAddress>{{.ListenAddress}}</listenAddress>
     <globalAnnounceServer>default</globalAnnounceServer>
     <globalAnnounceEnabled>false</globalAnnounceEnabled>
     <localAnnounceEnabled>false</localAnnounceEnabled>
