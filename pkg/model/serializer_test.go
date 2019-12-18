44a6d40eb01b5dda1ace782af7fb3a31b562dae2
Ramiro Berrelleza <rberrelleza@gmail.com>
2019-12-18 00:31:27 -0800

    serializer tests

--- pkg/model/serializer_test.go
+++ pkg/model/serializer_test.go
@@ -3,11 +3,129 @@ package model
 import (
 	"os"
 	"reflect"
+	"strings"
 	"testing"
 
 	yaml "gopkg.in/yaml.v2"
 )
 
+func TestRemoteForwardMashalling(t *testing.T) {
+	tests := []struct {
+		name      string
+		data      string
+		expected  RemoteForward
+		expectErr bool
+	}{
+		{
+			name:     "basic",
+			data:     "8080:9090",
+			expected: RemoteForward{Local: 9090, Remote: 8080},
+		},
+		{
+			name:     "equal",
+			data:     "8080:8080",
+			expected: RemoteForward{Local: 8080, Remote: 8080},
+		},
+		{
+			name:      "missing-part",
+			data:      "8080",
+			expectErr: true,
+		},
+		{
+			name:      "non-integer",
+			data:      "8080:foo",
+			expectErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var result RemoteForward
+			if err := yaml.Unmarshal([]byte(tt.data), &result); err != nil {
+				if tt.expectErr {
+					return
+				}
+
+				t.Fatal(err)
+			}
+
+			if !reflect.DeepEqual(result, tt.expected) {
+				t.Errorf("didn't unmarshal correctly. Actual '%+v', Expected '%+v'", result, tt.expected)
+			}
+
+			out, err := yaml.Marshal(result)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			outStr := string(out)
+			outStr = strings.TrimSuffix(outStr, "\n")
+
+			if !reflect.DeepEqual(outStr, tt.data) {
+				t.Errorf("didn't unmarshal correctly. Actual '%+v', Expected '%+v'", outStr, tt.data)
+			}
+		})
+	}
+}
+
+func TestForwardMashalling(t *testing.T) {
+	tests := []struct {
+		name      string
+		data      string
+		expected  Forward
+		expectErr bool
+	}{
+		{
+			name:     "basic",
+			data:     "8080:9090",
+			expected: Forward{Local: 8080, Remote: 9090},
+		},
+		{
+			name:     "equal",
+			data:     "8080:8080",
+			expected: Forward{Local: 8080, Remote: 8080},
+		},
+		{
+			name:      "missing-part",
+			data:      "8080",
+			expectErr: true,
+		},
+		{
+			name:      "non-integer",
+			data:      "8080:foo",
+			expectErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var result Forward
+			if err := yaml.Unmarshal([]byte(tt.data), &result); err != nil {
+				if tt.expectErr {
+					return
+				}
+
+				t.Fatal(err)
+			}
+
+			if !reflect.DeepEqual(result, tt.expected) {
+				t.Errorf("didn't unmarshal correctly. Actual '%+v', Expected '%+v'", result, tt.expected)
+			}
+
+			out, err := yaml.Marshal(result)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			outStr := string(out)
+			outStr = strings.TrimSuffix(outStr, "\n")
+
+			if !reflect.DeepEqual(outStr, tt.data) {
+				t.Errorf("didn't unmarshal correctly. Actual '%+v', Expected '%+v'", outStr, tt.data)
+			}
+		})
+	}
+}
 func TestEnvVarMashalling(t *testing.T) {
 	tests := []struct {
 		name     string
@@ -117,6 +235,143 @@ func TestVolumeMashalling(t *testing.T) {
 			_, err := yaml.Marshal(&v)
 			if err != nil {
 				t.Fatal(err)
+			}
+		})
+	}
+}
+
+func TestEnvVar_UnmarshalYAML(t *testing.T) {
+	type fields struct {
+		Name  string
+		Value string
+	}
+	type args struct {
+		unmarshal func(interface{}) error
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			e := &EnvVar{
+				Name:  tt.fields.Name,
+				Value: tt.fields.Value,
+			}
+			if err := e.UnmarshalYAML(tt.args.unmarshal); (err != nil) != tt.wantErr {
+				t.Errorf("EnvVar.UnmarshalYAML() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestForward_UnmarshalYAML(t *testing.T) {
+	type fields struct {
+		Local  int
+		Remote int
+	}
+	type args struct {
+		unmarshal func(interface{}) error
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			f := &Forward{
+				Local:  tt.fields.Local,
+				Remote: tt.fields.Remote,
+			}
+			if err := f.UnmarshalYAML(tt.args.unmarshal); (err != nil) != tt.wantErr {
+				t.Errorf("Forward.UnmarshalYAML() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestRemoteForward_UnmarshalYAML(t *testing.T) {
+	type fields struct {
+		Remote int
+		Local  int
+	}
+	type args struct {
+		unmarshal func(interface{}) error
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			f := &RemoteForward{
+				Remote: tt.fields.Remote,
+				Local:  tt.fields.Local,
+			}
+			if err := f.UnmarshalYAML(tt.args.unmarshal); (err != nil) != tt.wantErr {
+				t.Errorf("RemoteForward.UnmarshalYAML() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestResourceList_UnmarshalYAML(t *testing.T) {
+	type args struct {
+		unmarshal func(interface{}) error
+	}
+	tests := []struct {
+		name    string
+		r       *ResourceList
+		args    args
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := tt.r.UnmarshalYAML(tt.args.unmarshal); (err != nil) != tt.wantErr {
+				t.Errorf("ResourceList.UnmarshalYAML() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestVolume_UnmarshalYAML(t *testing.T) {
+	type fields struct {
+		SubPath   string
+		MountPath string
+	}
+	type args struct {
+		unmarshal func(interface{}) error
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			v := &Volume{
+				SubPath:   tt.fields.SubPath,
+				MountPath: tt.fields.MountPath,
+			}
+			if err := v.UnmarshalYAML(tt.args.unmarshal); (err != nil) != tt.wantErr {
+				t.Errorf("Volume.UnmarshalYAML() error = %v, wantErr %v", err, tt.wantErr)
 			}
 		})
 	}
