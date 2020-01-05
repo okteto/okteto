e4ae82eb8e9a55e8beaea0b2643d83e5a439fa51
Vikas <vikas.narula@paidtime.com>
2020-01-05 14:31:16 +0100

    'login showUrl' shows url, instead open in browser

--- cmd/login.go
+++ cmd/login.go
@@ -20,22 +20,28 @@ import (
 
 //Login starts the login handshake with github and okteto
 func Login() *cobra.Command {
+	showUrl := false
 	cmd := &cobra.Command{
 		Use:   "login [url]",
 		Short: "Login with Okteto",
 		RunE: func(cmd *cobra.Command, args []string) error {
 			oktetoURL := okteto.CloudURL
 			if len(args) > 0 {
-				u, err := url.Parse(args[0])
-				if err != nil {
-					return fmt.Errorf("malformed login URL")
+				fmt.Printf("args0: " + args[0])
+				if (args[0] == "showUrl") {
+					showUrl = true
+				} else {
+					u, err := url.Parse(args[0])
+					if err != nil {
+						return fmt.Errorf("malformed login URL")
+					}
+	
+					if u.Scheme == "" {
+						u.Scheme = "https"
+					}
+	
+					oktetoURL = u.String()
 				}
-
-				if u.Scheme == "" {
-					u.Scheme = "https"
-				}
-
-				oktetoURL = u.String()
 			}
 
 			log.Debugf("authenticating with %s", oktetoURL)
@@ -61,9 +67,10 @@ func Login() *cobra.Command {
 
 			authorizationURL := buildAuthorizationURL(handler.baseURL, handler.state, port)
 			fmt.Println("Authentication will continue in your default browser")
-			if err := open.Start(authorizationURL); err != nil {
-				fmt.Printf("Please open a browser and navigate to the following address:\n")
-				fmt.Println(authorizationURL)
+			if showUrl {
+				showAuthorizationURL(authorizationURL)
+			} else if err := open.Start(authorizationURL); err != nil {
+				showAuthorizationURL(authorizationURL)
 			}
 
 			ticker := time.NewTicker(5 * time.Minute)
@@ -136,6 +143,11 @@ func randToken() string {
 	b := make([]byte, 32)
 	rand.Read(b)
 	return base64.StdEncoding.EncodeToString(b)
+}
+
+func showAuthorizationURL() {
+	fmt.Printf("Please open a browser and navigate to the following address:\n")
+	fmt.Println(authorizationURL)
 }
 
 func buildAuthorizationURL(baseURL, state string, port int) string {
