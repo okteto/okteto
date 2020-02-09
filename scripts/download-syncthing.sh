b95a85328f931b846f2cdbf3e6db3b6c611913af
Ramiro Berrelleza <rberrelleza@gmail.com>
2020-02-09 14:14:58 -0800

    download syncthing script

--- scripts/download-syncthing.sh
+++ scripts/download-syncthing.sh
@@ -2,23 +2,27 @@
 set -ex
 VERSION=1.3.4
 
-wget -O syncthing-linux-amd64.tar.gz https://github.com/syncthing/syncthing/releases/download/v${VERSION}/syncthing-linux-amd64-v${VERSION}.tar.gz
-tar -zxvf syncthing-linux-amd64.tar.gz
-cp syncthing-linux-amd64-v${VERSION}/syncthing syncthing-Linux-x86_64
-rm -rf syncthing-linux-amd64*
+downloadLinux() {
+  prefix=$1
+  output=$2
+  wget -qO- https://github.com/syncthing/syncthing/releases/download/v${VERSION}/${prefix}-v${VERSION}.tar.gz | tar xvz ${prefix}-v${VERSION}/syncthing
+  mv ${prefix}-v${VERSION}/syncthing $output
+  rm -rf ${prefix}*
+  tar -czvf ${output}.tar.gz ${output}
+}
 
-wget -O syncthing-linux-arm64.tar.gz https://github.com/syncthing/syncthing/releases/download/v${VERSION}/syncthing-linux-arm64-v${VERSION}.tar.gz
-tar -zxvf syncthing-linux-arm64.tar.gz
-cp syncthing-linux-arm64-v${VERSION}/syncthing syncthing-Linux-arm64
-rm -rf syncthing-linux-arm64*
+downloadWindows() {
+  wget -O syncthing-windows-amd64.zip https://github.com/syncthing/syncthing/releases/download/v${VERSION}/syncthing-windows-amd64-v${VERSION}.zip
+  unzip -j syncthing-windows-amd64.zip syncthing-windows-amd64-v${VERSION}/syncthing.exe -d .
+  mv syncthing.exe syncthing-Windows-x86_64
+  rm -rf syncthing-windows-amd64*
+  tar -czvf syncthing-Windows-x86_64.tar.gz syncthing-Windows-x86_64
+}
 
-wget -O syncthing-macos-amd64.tar.gz https://github.com/syncthing/syncthing/releases/download/v${VERSION}/syncthing-macos-amd64-v${VERSION}.tar.gz
-tar -zxvf syncthing-macos-amd64.tar.gz
-cp syncthing-macos-amd64-v${VERSION}/syncthing syncthing-Darwin-x86_64
-rm -rf syncthing-macos-amd64*
+downloadLinux syncthing-linux-amd64 syncthing-Linux-x86_64
+downloadLinux syncthing-linux-arm64 syncthing-Linux-arm64
+downloadLinux syncthing-macos-amd64 syncthing-Darwin-x86_64
+downloadWindows
 
-wget -O syncthing-windows-amd64.zip https://github.com/syncthing/syncthing/releases/download/v${VERSION}/syncthing-windows-amd64-v${VERSION}.zip
-unzip syncthing-windows-amd64.zip
-cp syncthing-windows-amd64-v${VERSION}/syncthing.exe syncthing-Windows-x86_64
-rm -rf syncthing-windows-amd64*
+
 
