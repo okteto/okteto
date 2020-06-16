a2593f6cb1f53219b38f91e410bfab773d6c4e96
Ramiro Berrelleza <rberrelleza@gmail.com>
2020-06-15 18:36:52 -0700

    fix syntax
    Signed-off-by: Ramiro Berrelleza <rberrelleza@gmail.com>

--- integration/integration_test.go
+++ integration/integration_test.go
@@ -280,7 +280,7 @@ func TestAll(t *testing.T) {
 			}
 
 			if c != updatedContent {
-				t.Failf("expected updated content to be %s, got %s", updatedContent, c)
+				t.Fail(fmt.Sprintf("expected updated content to be %s, got %s", updatedContent, c))
 				time.Sleep(1 * time.Second)
 				continue
 			}
