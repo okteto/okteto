333348c5ade44631c78455a628434fa59a3a4d85
Pablo Chico de Guzman <pchico83@gmail.com>
2020-06-26 07:25:11 +0200

    "okteto init" always using okteto dev images (#960)
    Signed-off-by: Pablo Chico de Guzman <pchico83@gmail.com>

--- pkg/cmd/init/run.go
+++ pkg/cmd/init/run.go
@@ -22,13 +22,11 @@ import (
 	"github.com/okteto/okteto/pkg/k8s/pods"
 	"github.com/okteto/okteto/pkg/k8s/replicasets"
 	"github.com/okteto/okteto/pkg/k8s/services"
-	"github.com/okteto/okteto/pkg/log"
 	"github.com/okteto/okteto/pkg/model"
 	appsv1 "k8s.io/api/apps/v1"
 	apiv1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
 	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/rest"
 )
 
 var (
@@ -38,7 +36,7 @@ var (
 //SetDevDefaultsFromDeployment sets dev defaults from a running deployment
 func SetDevDefaultsFromDeployment(dev *model.Dev, d *appsv1.Deployment, container string) (*model.Dev, error) {
 	ctx := context.Background()
-	c, config, _, err := k8Client.GetLocal()
+	c, _, _, err := k8Client.GetLocal()
 	if err != nil {
 		return nil, err
 	}
@@ -48,9 +46,6 @@ func SetDevDefaultsFromDeployment(dev *model.Dev, d *appsv1.Deployment, containe
 		return nil, err
 	}
 
-	dev = setUserFromPod(ctx, dev, pod, container, config, c)
-	dev = setWorkDirFromPod(ctx, dev, pod, container, config, c)
-	dev = setCommandFromPod(ctx, dev, pod, container, config, c)
 	dev = setNameAndLabelsFromDeployment(ctx, dev, d)
 	dev = setAnnotationsFromDeployment(dev, d)
 	dev = setResourcesFromPod(dev, pod, container)
@@ -87,48 +82,6 @@ func getRunningPod(d *appsv1.Deployment, container string, c *kubernetes.Clients
 		}
 	}
 	return pod, nil
-}
-
-func setUserFromPod(ctx context.Context, dev *model.Dev, pod *apiv1.Pod, container string, config *rest.Config, c *kubernetes.Clientset) *model.Dev {
-	if dev.WorkDir != "" {
-		return dev
-	}
-	userID, err := pods.GetUserByPod(ctx, pod, container, config, c)
-	if err != nil {
-		log.Infof("error getting user of the deployment: %s", err)
-	}
-	if userID != 0 {
-		dev.SecurityContext = &model.SecurityContext{
-			RunAsUser: &userID,
-		}
-	}
-	return dev
-}
-
-func setWorkDirFromPod(ctx context.Context, dev *model.Dev, pod *apiv1.Pod, container string, config *rest.Config, c *kubernetes.Clientset) *model.Dev {
-	if dev.WorkDir != "" {
-		return dev
-	}
-	workdir, err := pods.GetWorkdirByPod(ctx, pod, container, config, c)
-	if err != nil {
-		log.Infof("error getting workdir of the deployment: %s", err)
-		workdir = "/okteto"
-	}
-
-	dev.WorkDir = workdir
-	return dev
-}
-
-func setCommandFromPod(ctx context.Context, dev *model.Dev, pod *apiv1.Pod, container string, config *rest.Config, c *kubernetes.Clientset) *model.Dev {
-	if dev.Command.Values != nil {
-		return dev
-	}
-	if pods.CheckIfBashIsAvailable(ctx, pod, container, config, c) {
-		dev.Command.Values = []string{"bash"}
-	} else {
-		dev.Command.Values = []string{"sh"}
-	}
-	return dev
 }
 
 func setForwardsFromPod(ctx context.Context, dev *model.Dev, pod *apiv1.Pod, c *kubernetes.Clientset) (*model.Dev, error) {
