5488db4a54cf1c38ae07e01cc410c9f14b8feea4
Ramiro Berrelleza <rberrelleza@gmail.com>
2020-08-17 01:08:42 -0700

    log quota error (#990)
    Signed-off-by: Ramiro Berrelleza <rberrelleza@gmail.com>

--- pkg/k8s/deployments/crud.go
+++ pkg/k8s/deployments/crud.go
@@ -89,6 +89,7 @@ func GetRevisionAnnotatedDeploymentOrFailed(dev *model.Dev, c *kubernetes.Client
 	for _, c := range d.Status.Conditions {
 		if c.Type == appsv1.DeploymentReplicaFailure && c.Reason == "FailedCreate" && c.Status == apiv1.ConditionTrue {
 			if strings.Contains(c.Message, "exceeded quota") {
+				log.Infof("%s: %s", errors.ErrQuota, c.Message)
 				return nil, errors.ErrQuota
 			}
 			return nil, fmt.Errorf(c.Message)
