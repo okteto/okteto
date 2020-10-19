68ecc7fed2af68edcc649ce6bdab9610a6908d7e
Pablo Chico de Guzman <pchico83@gmail.com>
2020-10-19 22:04:43 +0200

    Okteto label to every resource created by okteto (#1106)
    Signed-off-by: Pablo Chico de Guzman <pchico83@gmail.com>

--- pkg/k8s/volumes/translate.go
+++ pkg/k8s/volumes/translate.go
@@ -14,6 +14,7 @@
 package volumes
 
 import (
+	"github.com/okteto/okteto/pkg/k8s/labels"
 	"github.com/okteto/okteto/pkg/model"
 	apiv1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
@@ -24,6 +25,9 @@ func translate(dev *model.Dev) *apiv1.PersistentVolumeClaim {
 	pvc := &apiv1.PersistentVolumeClaim{
 		ObjectMeta: metav1.ObjectMeta{
 			Name: dev.GetVolumeName(),
+			Labels: map[string]string{
+				labels.DevLabel: "true",
+			},
 		},
 		Spec: apiv1.PersistentVolumeClaimSpec{
 			AccessModes: []apiv1.PersistentVolumeAccessMode{apiv1.ReadWriteOnce},
