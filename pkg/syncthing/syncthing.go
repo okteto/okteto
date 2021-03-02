aac5e7fd99bd9f22779b6b26a8583fbe18da4016
Ramiro Berrelleza <ramiro@okteto.com>
2021-03-02 11:02:29 -0800

    Fallback when we fail to get the name (#1299)
    * use github.com/mitchellh/go-ps to get name if it fails
    * skip EOF error when terminating the process
    
    Signed-off-by: Ramiro <rberrelleza@gmail.com>

--- pkg/syncthing/syncthing.go
+++ pkg/syncthing/syncthing.go
@@ -38,6 +38,7 @@ import (
 	yaml "gopkg.in/yaml.v2"
 
 	"github.com/google/uuid"
+	gops "github.com/mitchellh/go-ps"
 	"github.com/shirou/gopsutil/process"
 )
 
@@ -676,19 +677,45 @@ func (s *Syncthing) HardTerminate() error {
 	if err != nil {
 		return err
 	}
+
 	for _, p := range pList {
-		name, err := p.Name()
-		if err != nil {
-			log.Infof("error getting name for process %d: %s", p.Pid, err.Error())
+		if p.Pid == 0 {
 			continue
 		}
+
+		name, err := p.Name()
+		if err != nil {
+			// it's expected go get EOF if the process no longer exists at this point.
+			if err != io.EOF {
+				log.Infof("error getting name for process %d: %s", p.Pid, err.Error())
+			}
+			continue
+		}
+
+		// workaround until https://github.com/shirou/gopsutil/issues/1043 is fixed
+		if name == "" && runtime.GOOS == "darwin" && runtime.GOARCH == "arm64" {
+			pr, err := gops.FindProcess(int(p.Pid))
+			if err != nil {
+				log.Infof("error getting process  %d: %s", p.Pid, err.Error())
+				continue
+			}
+
+			name = pr.Executable()
+		}
+
+		if name == "" {
+			continue
+		}
+
 		if !strings.Contains(name, "syncthing") {
 			continue
 		}
+
 		cmdline, err := p.Cmdline()
 		if err != nil {
 			return err
 		}
+
 		if !strings.Contains(cmdline, fmt.Sprintf("-home %s", s.Home)) {
 			continue
 		}
