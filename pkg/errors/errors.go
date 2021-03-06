2aec9dcc0e07afd9ecb8814b321aa4be1b7ed545
Pablo Chico de Guzman <pchico83@gmail.com>
2021-03-06 15:00:48 +0100

    Fix windows test
    Signed-off-by: Pablo Chico de Guzman <pchico83@gmail.com>

--- pkg/errors/errors.go
+++ pkg/errors/errors.go
@@ -123,6 +123,7 @@ func IsTransient(err error) bool {
 		strings.Contains(err.Error(), "TLS handshake timeout"),
 		strings.Contains(err.Error(), "in the time allotted"),
 		strings.Contains(err.Error(), "broken pipe"),
+		strings.Contains(err.Error(), "No connection could be made"),
 		strings.Contains(err.Error(), "dial tcp: operation was canceled"),
 		strings.Contains(err.Error(), "network is unreachable"):
 		return true
