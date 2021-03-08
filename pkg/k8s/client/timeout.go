42806db18114e106b5fcb538c57b92fa2624bdaf
Pablo Chico de Guzman <pablo@okteto.com>
2021-03-08 11:44:25 +0100

    Configurable kubernetes timeout via env var (#1320)
    Signed-off-by: Pablo Chico de Guzman <pchico83@gmail.com>

---
+++ pkg/k8s/client/timeout.go
@@ -0,0 +1,33 @@
+package client
+
+import (
+	"os"
+	"sync"
+	"time"
+
+	"github.com/okteto/okteto/pkg/log"
+)
+
+var timeout time.Duration
+var tOnce sync.Once
+
+func getKubernetesTimeout() time.Duration {
+	tOnce.Do(func() {
+		timeout = 10 * time.Second
+		t, ok := os.LookupEnv("OKTETO_KUBERNETES_TIMEOUT")
+		if !ok {
+			return
+		}
+
+		parsed, err := time.ParseDuration(t)
+		if err != nil {
+			log.Infof("'%s' is not a valid duration, ignoring", t)
+			return
+		}
+
+		log.Infof("OKTETO_KUBERNETES_TIMEOUT applied: '%s'", parsed.String())
+		timeout = parsed
+	})
+
+	return timeout
+}
