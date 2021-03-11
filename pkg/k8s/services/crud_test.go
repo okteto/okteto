9f25003a4e7fabfdca4a8c9473d6d4551ffa7eb6
jLopezbarb <javier@okteto.com>
2021-03-11 12:28:23 +0100

    Add test for getting svc by label selector
    Signed-off-by: jLopezbarb <javier@okteto.com>

--- pkg/k8s/services/crud_test.go
+++ pkg/k8s/services/crud_test.go
@@ -18,6 +18,7 @@ import (
 	"testing"
 
 	"github.com/okteto/okteto/pkg/errors"
+	"github.com/okteto/okteto/pkg/k8s/labels"
 	apiv1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes/fake"
@@ -54,4 +55,85 @@ func TestGet(t *testing.T) {
 	if !errors.IsNotFound(err) {
 		t.Fatalf("expected not found error got: %s", err)
 	}
+}
+
+func TestGetNameBySelector(t *testing.T) {
+
+	ctx := context.Background()
+	svcNameToFind := "svc"
+
+	tests := []struct {
+		name          string
+		userLabels    map[string]string
+		svcLabels     map[string]string
+		expected      string
+		expectedError bool
+	}{
+		{
+			"full-match-one-only-label-get-correct-name",
+			map[string]string{"app": "db"},
+			map[string]string{"app": "db"},
+			svcNameToFind,
+			false,
+		},
+		{
+			"full-match-labels-get-correct-name",
+			map[string]string{"app": "db", "stage": "prod"},
+			map[string]string{"app": "db", "stage": "prod"},
+			svcNameToFind,
+			false,
+		},
+		{
+			"partial-match-labels-get-correct-name",
+			map[string]string{"app": "db"},
+			map[string]string{"app": "db", "stage": "prod"},
+			svcNameToFind,
+			false,
+		},
+		{
+			"partial-match-labels-when-multiple-services-same-labels-get-error",
+			map[string]string{"stage": "prod"},
+			map[string]string{"app": "db", "stage": "prod"},
+			"",
+			true,
+		},
+		{
+			"none-match-labels",
+			map[string]string{"stage": "dev"},
+			map[string]string{"app": "db", "stage": "prod"},
+			"",
+			true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			svcToFind := &apiv1.Service{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      svcNameToFind,
+					Namespace: "test",
+					Labels:    tt.svcLabels,
+				},
+			}
+			anotherService := &apiv1.Service{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "error-svc",
+					Namespace: "test",
+					Labels:    map[string]string{"app": "api", "stage": "prod"},
+				},
+			}
+			clientset := fake.NewSimpleClientset(svcToFind, anotherService)
+			userSelector := labels.TransformLabelsToSelector(tt.userLabels)
+			svcName, err := GetServiceNameByLabel(ctx, svcToFind.GetNamespace(), clientset, userSelector)
+			if err != nil {
+				if !tt.expectedError {
+					t.Fatalf("unexpected error while getting service name: %s", err.Error())
+				}
+				return
+			}
+			if svcName != svcNameToFind {
+				t.Errorf("Not correct service found. Found %s but expected %s", svcName, svcNameToFind)
+			}
+		})
+	}
+
 }
