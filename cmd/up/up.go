88026a7753f4b5b1692ba793ce1226a80a61278d
jLopezbarb <javier@okteto.com>
2021-03-15 14:42:28 +0100

    Remove leading and tailing spaces
    Signed-off-by: jLopezbarb <javier@okteto.com>

--- cmd/up/up.go
+++ cmd/up/up.go
@@ -410,7 +410,7 @@ func (up *upContext) activate(autoDeploy, build bool) error {
 		output := <-up.cleaned
 		log.Debugf("clean command output: %s", output)
 
-		outByCommand := strings.Split(output, "\n")
+		outByCommand := strings.Split(strings.TrimSpace(output), "\n")
 		if len(outByCommand) >= 2 {
 			version, watches := outByCommand[0], outByCommand[1]
 
@@ -431,11 +431,6 @@ func (up *upContext) activate(autoDeploy, build bool) error {
 				log.Yellow("Please consider upgrading your init container image %s with the content of %s", up.Dev.InitContainer.Image, model.OktetoBinImageTag)
 				log.Infof("Manifest init image %s instead of actual init image (%s)", up.Dev.InitContainer.Image, model.OktetoBinImageTag)
 			}
-		} else {
-			log.Yellow("Couldn't get init container image version and watches configuration.")
-			log.Yellow("This can affect file synchronization performance and the application startup.")
-			log.Yellow("Please consider upgrading your init container image with the content of %s", model.OktetoBinImageTag)
-			log.Yellow("and setting the value of /proc/sys/fs/inotify/max_user_watches in your cluster nodes")
 		}
 		printDisplayContext(up.Dev)
 		up.CommandResult <- up.runCommand(ctx)
