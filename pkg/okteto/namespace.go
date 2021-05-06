07a59f104639c7498a7fdb66ad720172aa1bc46b
Javier López Barba <javier@okteto.com>
2021-05-06 13:11:40 +0200

    Validate namespace name before creating it (#1474)
    * Uppercase first character
    
    Signed-off-by: Javier López Barba <javier@okteto.com>
    
    * Add namespace validation before creating it
    
    Signed-off-by: Javier López Barba <javier@okteto.com>
    
    * Add validate namespace tests
    
    Signed-off-by: Javier López Barba <javier@okteto.com>
    
    * Rewrite error mesage
    
    Signed-off-by: Javier López Barba <javier@okteto.com>

--- pkg/okteto/namespace.go
+++ pkg/okteto/namespace.go
@@ -16,6 +16,14 @@ package okteto
 import (
 	"context"
 	"fmt"
+	"regexp"
+
+	"github.com/okteto/okteto/pkg/errors"
+)
+
+const (
+	// Maximum number of characters allowed in a namespace name
+	MAX_ALLOWED_CHARS = 63
 )
 
 // CreateBody top body answer
@@ -41,6 +49,9 @@ type Namespace struct {
 
 // CreateNamespace creates a namespace
 func CreateNamespace(ctx context.Context, namespace string) (string, error) {
+	if err := validateNamespace(namespace); err != nil {
+		return "", err
+	}
 	q := fmt.Sprintf(`mutation{
 		createSpace(name: "%s"){
 			id
@@ -109,4 +120,21 @@ func DeleteNamespace(ctx context.Context, namespace string) error {
 
 	var body DeleteBody
 	return query(ctx, q, &body)
+}
+
+func validateNamespace(namespace string) error {
+	if len(namespace) > MAX_ALLOWED_CHARS {
+		return errors.UserError{
+			E:    fmt.Errorf("Invalid namespace name."),
+			Hint: "Namespace name must be shorter than 63 characters.",
+		}
+	}
+	nameValidationRegex := regexp.MustCompile("^[a-z0-9]([-a-z0-9]*[a-z0-9])?$")
+	if !nameValidationRegex.MatchString(namespace) {
+		return errors.UserError{
+			E:    fmt.Errorf("Invalid namespace name."),
+			Hint: "Namespace name must consist of lower case alphanumeric characters or '-', and must start and end with an alphanumeric character",
+		}
+	}
+	return nil
 }
