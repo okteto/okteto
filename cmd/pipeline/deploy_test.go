61fc7dd5ade255373850a4230ae62f9b345d983a
Javier López Barba <javier@okteto.com>
2021-05-11 19:26:33 +0200

    Fix integration tests
    Signed-off-by: Javier López Barba <javier@okteto.com>

--- cmd/pipeline/deploy_test.go
+++ cmd/pipeline/deploy_test.go
@@ -12,7 +12,6 @@ import (
 	"github.com/go-git/go-git/v5/config"
 	"github.com/go-git/go-git/v5/plumbing"
 	"github.com/go-git/go-git/v5/plumbing/object"
-	"github.com/okteto/okteto/cmd/utils"
 )
 
 func Test_getRepositoryURL(t *testing.T) {
@@ -68,7 +67,7 @@ func Test_getRepositoryURL(t *testing.T) {
 			}
 			defer os.RemoveAll(dir)
 
-			if _, err := utils.GetRepositoryURL(context.TODO(), dir); err == nil {
+			if _, err := GetRepositoryURL(context.TODO(), dir); err == nil {
 				t.Fatal("expected error when there's no github repo")
 			}
 
@@ -83,7 +82,7 @@ func Test_getRepositoryURL(t *testing.T) {
 				}
 			}
 
-			url, err := utils.GetRepositoryURL(context.TODO(), dir)
+			url, err := GetRepositoryURL(context.TODO(), dir)
 			if tt.expectError {
 				if err == nil {
 					t.Error("expected error when calling getRepositoryURL")
@@ -115,7 +114,7 @@ func Test_getBranch(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	_, err = utils.GetBranch(context.TODO(), dir)
+	_, err = GetBranch(context.TODO(), dir)
 	if err == nil {
 		t.Fatal("expected no-branch error")
 	}
@@ -161,7 +160,7 @@ func Test_getBranch(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	b, err := utils.GetBranch(context.TODO(), dir)
+	b, err := GetBranch(context.TODO(), dir)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -176,7 +175,7 @@ func Test_getBranch(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	if _, err := utils.GetBranch(context.TODO(), dir); err == nil {
+	if _, err := GetBranch(context.TODO(), dir); err == nil {
 		t.Fatal("didn't fail when getting a non branch")
 	}
 }
