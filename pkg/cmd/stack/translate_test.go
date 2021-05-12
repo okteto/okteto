4626fc4d24be57adeee1e571ec627c6724c89e2e
Javier López Barba <javier@okteto.com>
2021-05-12 13:32:16 +0200

    Fix tests
    Signed-off-by: Javier López Barba <javier@okteto.com>

--- pkg/cmd/stack/translate_test.go
+++ pkg/cmd/stack/translate_test.go
@@ -326,7 +326,7 @@ func Test_translateStatefulSet(t *testing.T) {
 	initContainer := apiv1.Container{
 		Name:    fmt.Sprintf("init-%s", "svcName"),
 		Image:   "busybox",
-		Command: []string{"sh", "-c", "chmod 777 /data/*"},
+		Command: []string{"sh", "-c", "chmod 777 -R /data"},
 		VolumeMounts: []apiv1.VolumeMount{
 			{
 				MountPath: "/data",
