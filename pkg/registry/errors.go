c4ea86f9644482041cb5f45b69dba0f6590364bf
Javier López Barba <javier@okteto.com>
2021-05-17 15:18:16 +0200

    Retry if build fails with large context
    Signed-off-by: Javier López Barba <javier@okteto.com>

--- pkg/registry/errors.go
+++ pkg/registry/errors.go
@@ -59,6 +59,8 @@ func IsTransientError(err error) bool {
 		return true
 	case strings.Contains(err.Error(), "failed to do request") && strings.Contains(err.Error(), "tls: use of closed connection"):
 		return true
+	case strings.Contains(err.Error(), "Canceled desc") && strings.Contains(err.Error(), "the client connection is closing"):
+		return true
 	default:
 		return false
 	}
