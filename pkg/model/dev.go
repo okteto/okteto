a7b75f6cb97c1e419d6765052fa4d6be4e75ee3d
Pablo Chico de Guzman <pablo@okteto.com>
2021-05-26 15:06:39 +0200

    Only initialize the okteto volume from the dev image on first pod execution (#1568)
    Signed-off-by: Pablo Chico de Guzman <pchico83@gmail.com>

--- pkg/model/dev.go
+++ pkg/model/dev.go
@@ -108,7 +108,7 @@ const (
 
 var (
 	//OktetoBinImageTag image tag with okteto internal binaries
-	OktetoBinImageTag = "okteto/bin:1.2.29"
+	OktetoBinImageTag = "okteto/bin:1.3.0"
 
 	errBadName = fmt.Errorf("Invalid name: must consist of lower case alphanumeric characters or '-', and must start and end with an alphanumeric character")
 
