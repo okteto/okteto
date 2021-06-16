e413ffe86b45f14a05bd356e8cca1aa0a3d2ab1c
Javier López Barba <javier@okteto.com>
2021-06-16 16:54:15 +0200

    Trim long messages while spinning
    Signed-off-by: Javier López Barba <javier@okteto.com>

--- cmd/utils/spinner.go
+++ cmd/utils/spinner.go
@@ -23,6 +23,7 @@ import (
 
 	sp "github.com/briandowns/spinner"
 	"github.com/okteto/okteto/pkg/log"
+	"golang.org/x/crypto/ssh/terminal"
 )
 
 var spinnerSupport bool
@@ -38,6 +39,16 @@ func NewSpinner(suffix string) *Spinner {
 	s := sp.New(sp.CharSets[14], 100*time.Millisecond)
 	s.HideCursor = true
 	s.Suffix = fmt.Sprintf(" %s", suffix)
+	s.FinalMSG = s.Suffix
+	s.PreUpdate = func(s *sp.Spinner) {
+		width, _, _ := terminal.GetSize(int(os.Stdout.Fd()))
+		if width > 4 && len(s.FinalMSG) > width {
+			s.Suffix = s.FinalMSG[:width-5] + "..."
+		} else {
+			s.Suffix = s.FinalMSG
+		}
+	}
+
 	return &Spinner{
 		sp: s,
 	}
@@ -60,6 +71,9 @@ func loadBoolean(k string) bool {
 //Start starts the spinner
 func (p *Spinner) Start() {
 	if spinnerSupport {
+		if p.sp.FinalMSG == "" {
+			p.sp.FinalMSG = p.sp.Suffix
+		}
 		p.sp.Start()
 	} else {
 		fmt.Println(strings.TrimSpace(p.sp.Suffix))
@@ -68,6 +82,9 @@ func (p *Spinner) Start() {
 
 //Stop stops the spinner
 func (p *Spinner) Stop() {
+	if p.sp.FinalMSG != "" {
+		p.sp.FinalMSG = ""
+	}
 	if spinnerSupport {
 		p.sp.Stop()
 	}
@@ -76,6 +93,7 @@ func (p *Spinner) Stop() {
 //Update updates the spinner message
 func (p *Spinner) Update(text string) {
 	p.sp.Suffix = fmt.Sprintf(" %s", ucFirst(text))
+	p.sp.FinalMSG = fmt.Sprintf(" %s", ucFirst(text))
 	if !spinnerSupport {
 		fmt.Println(strings.TrimSpace(p.sp.Suffix))
 	}
