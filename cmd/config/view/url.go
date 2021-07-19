e8950fd1cc95fe9e6c12ecf3e007ae3360e478b1
Javier López Barba <javier@okteto.com>
2021-07-19 17:39:16 +0200

    Fix ci tests
    Signed-off-by: Javier López Barba <javier@okteto.com>

--- cmd/config/view/url.go
+++ cmd/config/view/url.go
@@ -32,7 +32,7 @@ func URL(ctx context.Context) *cobra.Command {
 		Short: "Returns the Okteto URL where the current user is authenticated",
 		RunE: func(cmd *cobra.Command, args []string) error {
 			t, err := okteto.GetToken()
-			if err != nil || t.Token != "" {
+			if err != nil {
 				log.Infof("error getting okteto token: %s", err.Error())
 				return errors.ErrNotLogged
 			}
