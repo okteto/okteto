3c3582120a9de373dbe357424d5c74ebb07c5320
Javier López Barba <javier@okteto.com>
2021-08-19 14:29:44 +0200

    Merge with master
    Signed-off-by: Javier López Barba <javier@okteto.com>

--- main.go
+++ main.go
@@ -27,6 +27,7 @@ import (
 	"github.com/okteto/okteto/cmd/preview"
 	"github.com/okteto/okteto/cmd/stack"
 	"github.com/okteto/okteto/cmd/up"
+	"github.com/okteto/okteto/cmd/utils"
 	"github.com/okteto/okteto/pkg/config"
 	"github.com/okteto/okteto/pkg/errors"
 	"github.com/okteto/okteto/pkg/log"
