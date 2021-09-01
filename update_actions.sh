a689b210873140330e04710d421f3aebc3a0cd29
Javier López Barba <javier@okteto.com>
2021-09-01 09:32:59 +0200

    Fix trailing whitespaces
    Signed-off-by: Javier López Barba <javier@okteto.com>

--- update_actions.sh
+++ update_actions.sh
@@ -9,33 +9,32 @@ if [ -z "$VERSION" ]; then
 fi
 
 actionsRepos=(delete-namespace
-              build
-              destroy-preview
-              deploy-preview
-              deploy-stack
-              namespace
-              pipeline
-              push
-              create-namespace
-              destroy-pipeline
-              login
-              destroy-stack
-              apply)
+        build
+        destroy-preview
+        deploy-preview
+        deploy-stack
+        namespace
+        pipeline
+        push
+        create-namespace
+        destroy-pipeline
+        login
+        destroy-stack
+        apply)
               
-for repo in "${actionsRepos[@]}"
-do
-    echo "$repo"
-    git clone --depth 1 git@github.com:okteto/"$repo".git
-    pushd "$repo"
-    git config user.name "okteto"
-    git config user.email "ci@okteto.com"
-    sed -iE 's_FROM\ okteto\/okteto\:latest_FROM\ okteto\/okteto\:'"$VERSION"'_' Dockerfile
-    sed -iE 's_FROM\ okteto\/okteto\:[[:digit:]]*\.[[:digit:]]*\.[[:digit:]]*_FROM\ okteto\/okteto\:'"$VERSION"'_' Dockerfile
-    git add Dockerfile
-    git commit -m "release $VERSION"
-    git push git@github.com:okteto/"$repo".git master
-    git --no-pager log -1
-    ghr -u "${CIRCLE_PROJECT_USERNAME}" -token "$GITHUB_TOKEN" -replace "$VERSION"
-    popd
-    rm -rf "$repo"
+for repo in "${actionsRepos[@]}"; do
+        echo "$repo"
+        git clone --depth 1 git@github.com:okteto/"$repo".git
+        pushd "$repo"
+        git config user.name "okteto"
+        git config user.email "ci@okteto.com"
+        sed -iE 's_FROM\ okteto\/okteto\:latest_FROM\ okteto\/okteto\:'"$VERSION"'_' Dockerfile
+        sed -iE 's_FROM\ okteto\/okteto\:[[:digit:]]*\.[[:digit:]]*\.[[:digit:]]*_FROM\ okteto\/okteto\:'"$VERSION"'_' Dockerfile
+        git add Dockerfile
+        git commit -m "release $VERSION"
+        git push git@github.com:okteto/"$repo".git master
+        git --no-pager log -1
+        ghr -u "${CIRCLE_PROJECT_USERNAME}" -token "$GITHUB_TOKEN" -replace "$VERSION"
+        popd
+        rm -rf "$repo"
 done
