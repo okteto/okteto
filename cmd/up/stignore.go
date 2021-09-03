f77eead255899de292f56816c07cd823b2754da7
Javier López Barba <javier@okteto.com>
2021-09-03 15:56:52 +0200

    Fix multiple sync folders .stignore file
    Signed-off-by: Javier López Barba <javier@okteto.com>

--- cmd/up/stignore.go
+++ cmd/up/stignore.go
@@ -46,7 +46,7 @@ func addStignoreSecrets(dev *model.Dev) error {
 		defer infile.Close()
 		reader := bufio.NewReader(infile)
 
-		stignoreName := fmt.Sprintf("stignore-%d", i+1)
+		stignoreName := fmt.Sprintf(".stignore-%d", i+1)
 		transformedStignorePath := filepath.Join(config.GetDeploymentHome(dev.Namespace, dev.Name), stignoreName)
 		outfile, err := os.OpenFile(transformedStignorePath, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
 		if err != nil {
