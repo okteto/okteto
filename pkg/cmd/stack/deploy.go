bb95acf247a0d4a8512334025b262674f22d2ad2
Javier López Barba <javier@okteto.com>
2021-09-21 12:51:46 +0200

    Return interrupt signal error and set cfg to error
    Signed-off-by: Javier López Barba <javier@okteto.com>

--- pkg/cmd/stack/deploy.go
+++ pkg/cmd/stack/deploy.go
@@ -165,7 +165,7 @@ func deploy(ctx context.Context, s *model.Stack, c *kubernetes.Clientset, config
 	case <-stop:
 		log.Infof("CTRL+C received, starting shutdown sequence")
 		spinner.Stop()
-		os.Exit(130)
+		return fmt.Errorf("interrupt signal received")
 	case err := <-exit:
 		if err != nil {
 			log.Infof("exit signal received due to error: %s", err)
