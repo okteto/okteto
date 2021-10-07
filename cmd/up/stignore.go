199726495f7d4786e7b6314fbe649de89a6895e4
Javier López Barba <javier@okteto.com>
2021-10-07 12:50:46 +0200

    Fix stignore error
    Signed-off-by: Javier López Barba <javier@okteto.com>

--- cmd/up/stignore.go
+++ cmd/up/stignore.go
@@ -45,7 +45,7 @@ func addStignoreSecrets(dev *model.Dev) error {
 		defer infile.Close()
 		reader := bufio.NewReader(infile)
 
-		stignoreName := fmt.Sprintf("stignore-%d", i+1)
+		stignoreName := fmt.Sprintf(".stignore-%d", i+1)
 		transformedStignorePath := filepath.Join(config.GetAppHome(dev.Namespace, dev.Name), stignoreName)
 		outfile, err := os.OpenFile(transformedStignorePath, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
 		if err != nil {
