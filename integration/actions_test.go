acc4cdd5d4b7492f92cbe87a62162df0da759883
Javier López Barba <javier@okteto.com>
2021-10-28 18:15:38 +0200

    Fix apply e2e tests
    Signed-off-by: Javier López Barba <javier@okteto.com>

--- integration/actions_test.go
+++ integration/actions_test.go
@@ -100,6 +100,13 @@ func TestApplyPipeline(t *testing.T) {
 	if err := executeCreateNamespaceAction(ctx, namespace); err != nil {
 		t.Fatalf("Create namespace action failed: %s", err.Error())
 	}
+	oktetoPath, err := getOktetoPath(ctx)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err := updateKubeConfig(oktetoPath); err != nil {
+		t.Fatal(err)
+	}
 
 	if err := executeApply(ctx, namespace); err != nil {
 		t.Fatalf("Apply action failed: %s", err.Error())
