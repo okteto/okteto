9e2f558e697b89e3e94e16b3a0c11b5e9f97c4fb
RinkiyaKeDad <arshsharma461@gmail.com>
2021-12-03 12:31:15 +0530

    fix function in error message for logs
    Signed-off-by: RinkiyaKeDad <arshsharma461@gmail.com>

--- cmd/up/up.go
+++ cmd/up/up.go
@@ -181,7 +181,7 @@ func Up() *cobra.Command {
 				log.Infof("error deleting deprecated volume: %v", err)
 			}
 
-			err = fmt.Errorf("%w\n    Find additional logs at: %s/okteto.log", err, config.GetOktetoHome())
+			err = fmt.Errorf("%w\n    Find additional logs at: %s/okteto.log", err, config.GetAppHome(dev.Namespace, dev.Name))
 
 			return err
 		},
