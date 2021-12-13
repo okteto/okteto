3fa928b5c02e960fe61a7179d439c4f3c75fa340
Javier López Barba <javier@okteto.com>
2021-12-13 15:07:12 +0100

    Return user friendly error if secret file is not found
    Signed-off-by: Javier López Barba <javier@okteto.com>

--- pkg/model/serializer.go
+++ pkg/model/serializer.go
@@ -634,7 +634,7 @@ func (l Lifecycle) MarshalYAML() (interface{}, error) {
 func checkFileAndNotDirectory(path string) error {
 	fileInfo, err := os.Stat(path)
 	if err != nil {
-		return err
+		return fmt.Errorf("'%s' not found. Please make sure the file exists", path)
 	}
 	if fileInfo.Mode().IsRegular() {
 		return nil
