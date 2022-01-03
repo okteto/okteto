7a7fa86362882fae820ad63382bcf9e5ab335b50
Javier López Barba <javier@okteto.com>
2022-01-03 12:36:56 +0100

    fix: windows build
    Signed-off-by: Javier López Barba <javier@okteto.com>

--- cmd/utils/executor/executor-windows.go
+++ cmd/utils/executor/executor-windows.go
@@ -24,7 +24,7 @@ import (
 	"os/exec"
 )
 
-func (e *ttyExecutorDisplayer) startCommand(cmd *exec.Cmd) (io.Reader, error) {
+func (e *ttyExecutorDisplayer) startCommand(cmd *exec.Cmd) error {
 	e.screenbuf = screenbuf.New(os.Stdout)
 
 	stdoutReader, err := cmd.StdoutPipe()
