261c3da1fcb012a7e1ac036fb8e7c6f3eee955da
Teresa Romero <teresa@okteto.com>
2022-01-19 12:46:22 +0100

    test: refactor test structure and added subtests
    Signed-off-by: Teresa Romero <teresa@okteto.com>

--- integration/deploy_manifest_test.go
+++ integration/deploy_manifest_test.go
@@ -20,7 +20,6 @@ import (
 	"context"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -32,10 +31,6 @@ import (
 	"github.com/okteto/okteto/pkg/okteto"
 	"github.com/okteto/okteto/pkg/registry"
 )
-
-// okteto deploy - build the images
-// okteto deploy  - does not build the images
-// okteto deploy --build - rebuilds the images
 
 func TestDeployFromManifest(t *testing.T) {
 	ctx := context.Background()
@@ -59,47 +54,40 @@ func TestDeployFromManifest(t *testing.T) {
 	expectedImage := fmt.Sprintf("%s/%s/%s-app:okteto", okteto.Context().Registry, testNamespace, repoDir)
 	t.Logf("expected image tag to be built and deployed: %s", expectedImage)
 
-	t.Run(testID, func(t *testing.T) {
+	startNamespace := getCurrentNamespace()
 
-		startNamespace := getCurrentNamespace()
-		defer changeToNamespace(ctx, oktetoPath, startNamespace)
+	if err := createNamespace(ctx, oktetoPath, testNamespace); err != nil {
+		t.Fatal(err)
+	}
 
-		if err := createNamespace(ctx, oktetoPath, testNamespace); err != nil {
-			t.Fatal(err)
-		}
-		defer deleteNamespace(ctx, oktetoPath, testNamespace)
+	if err := cloneGitRepo(ctx, gitRepo); err != nil {
+		t.Fatal(err)
+	}
 
-		if err := cloneGitRepo(ctx, gitRepo); err != nil {
-			t.Fatal(err)
-		}
-		defer deleteGitRepo(ctx, repoDir)
+	cwd, err := os.Getwd()
+	if err != nil {
+		t.Fatal(err)
+	}
 
-		cwd, err := os.Getwd()
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		manifestPath := filepath.Join(cwd, repoDir)
-		t.Logf("manifestPath: %s", manifestPath)
-
-		manifestContent := `build:
+	manifestPath := filepath.Join(cwd, repoDir)
+	manifestContent := `build:
   app:
     context: .
 deploy:
   - helm upgrade --install hello-world chart --set app.image=${build.app.image}`
 
-		if err := writeFile(manifestPath, manifestFilename, manifestContent); err != nil {
-			t.Fatal(err)
-		}
+	if err := writeFile(manifestPath, manifestFilename, manifestContent); err != nil {
+		t.Fatal(err)
+	}
 
-		chartContent := `apiVersion: v2
+	chartContent := `apiVersion: v2
 name: hello-world
 description: A React application in Kubernetes
 type: application
 version: 0.1.0
 appVersion: 1.0.0`
 
-		templateContent := `apiVersion: apps/v1
+	templateContent := `apiVersion: apps/v1
 kind: Deployment
 metadata:
   name: hello-world
@@ -132,30 +120,60 @@ spec:
   selector:
     app: hello-world`
 
-		pathToChartDir := filepath.Join(cwd, repoDir, chartDir)
-		if err := os.Mkdir(pathToChartDir, 0777); err != nil {
-			t.Fatal(err)
-		}
-		if err := writeFile(pathToChartDir, chartFilename, chartContent); err != nil {
-			t.Fatal(err)
-		}
+	pathToChartDir := filepath.Join(cwd, repoDir, chartDir)
+	if err := os.Mkdir(pathToChartDir, 0777); err != nil {
+		t.Fatal(err)
+	}
+	if err := writeFile(pathToChartDir, chartFilename, chartContent); err != nil {
+		t.Fatal(err)
+	}
 
-		pathToTemplateDir := filepath.Join(pathToChartDir, "templates")
-		if err := os.Mkdir(pathToTemplateDir, 0777); err != nil {
-			t.Fatal(err)
-		}
-		if err := writeFile(pathToTemplateDir, templateFilename, templateContent); err != nil {
-			t.Fatal(err)
-		}
+	pathToTemplateDir := filepath.Join(pathToChartDir, "templates")
+	if err := os.Mkdir(pathToTemplateDir, 0777); err != nil {
+		t.Fatal(err)
+	}
+	if err := writeFile(pathToTemplateDir, templateFilename, templateContent); err != nil {
+		t.Fatal(err)
+	}
 
-		cmd := exec.Command(oktetoPath, "deploy", "-l", "debug")
-		cmd.Env = append(os.Environ(), "OKTETO_ENABLE_MANIFEST_V2=true")
-		cmd.Dir = repoDir
-		o, err := cmd.CombinedOutput()
+	t.Cleanup(func() {
+		changeToNamespace(ctx, oktetoPath, startNamespace)
+		deleteNamespace(ctx, oktetoPath, testNamespace)
+		deleteGitRepo(ctx, repoDir)
+	})
+
+	t.Run("okteto deploy should build images if not exists at registry", func(t *testing.T) {
+
+		output, err := runOktetoDeploy(oktetoPath, repoDir)
 		if err != nil {
-			t.Fatalf("okteto deploy failed: %s - %s", string(o), err)
+			t.Fatalf(err.Error())
 		}
-		output := string(o)
+
+		imageWithDigest, err := registry.GetImageTagWithDigest(expectedImage)
+		if err != nil {
+			t.Fatalf(err.Error())
+		}
+		sha := strings.SplitN(imageWithDigest, "@", 2)[1]
+
+		if err := expectBuiltImageNotFound(output); err != nil {
+			t.Fatalf(err.Error())
+		}
+
+		if err := expectHelmInstallation(output, releaseName, testNamespace); err != nil {
+			t.Fatalf(err.Error())
+		}
+
+		if err := expectEnvSetting(output, testNamespace, repoDir, sha); err != nil {
+			t.Fatalf(err.Error())
+		}
+
+		if err := expectAppToBeRunning(releaseName, testNamespace); err != nil {
+			t.Fatalf(err.Error())
+		}
+
+	})
+
+	t.Run("okteto deploy should not build images if already at registry", func(t *testing.T) {
 
 		imageWithDigest, err := registry.GetImageTagWithDigest(expectedImage)
 		if err != nil {
@@ -163,54 +181,148 @@ spec:
 		}
 		sha := strings.SplitN(imageWithDigest, "@", 2)[1]
 
-		if ok := expectBuiltImageNotFound(output); !ok {
-			t.Fatalf("expected to build image before deploy")
+		output, err := runOktetoDeploy(oktetoPath, repoDir)
+		if err != nil {
+			t.Fatalf(err.Error())
 		}
 
-		if ok := expectHelmInstallation(output, releaseName, testNamespace); !ok {
-			t.Fatalf("expected helm first installation")
+		if err := expectImageFoundSkippingBuild(output); err != nil {
+			t.Fatalf(err.Error())
 		}
 
-		if ok := expectEnvSetting(output, testNamespace, repoDir, sha); !ok {
+		if err := expectHelmUpgrade(output, releaseName, testNamespace, "2"); err != nil {
+			t.Fatalf(err.Error())
+		}
+
+		if err := expectEnvSetting(output, testNamespace, repoDir, sha); err != nil {
+			t.Fatalf(err.Error())
+		}
+
+		if err := expectAppToBeRunning(releaseName, testNamespace); err != nil {
+			t.Fatalf(err.Error())
+		}
+
+	})
+
+	t.Run("okteto deploy --build should force the build even if exist", func(t *testing.T) {
+
+		imageWithDigest, err := registry.GetImageTagWithDigest(expectedImage)
+		if err != nil {
+			t.Fatalf("image is not at registry: %s", err.Error())
+		}
+		sha := strings.SplitN(imageWithDigest, "@", 2)[1]
+
+		output, err := runOktetoDeployForceBuild(oktetoPath, repoDir)
+		if err != nil {
+			t.Fatalf(err.Error())
+		}
+
+		if err := expectForceBuild(output); err != nil {
+			t.Fatalf(err.Error())
+		}
+
+		if err := expectHelmUpgrade(output, releaseName, testNamespace, "3"); err != nil {
+			t.Fatalf("expected helm upgrade")
+		}
+
+		if err := expectEnvSetting(output, testNamespace, repoDir, sha); err != nil {
 			t.Fatalf("expected to environment variables to be set")
 		}
 
-		if ok := expectAppToBeRunning(releaseName, testNamespace); !ok {
+		if err := expectAppToBeRunning(releaseName, testNamespace); err != nil {
 			t.Fatalf("expected the app to be running with exact content")
 		}
 
 	})
 }
 
-func expectBuiltImageNotFound(output string) bool {
-	return strings.Contains(output, "image not found, building image")
+func runOktetoDeploy(oktetoPath, repoDir string) (string, error) {
+	cmd := exec.Command(oktetoPath, "deploy", "-l", "debug")
+	cmd.Env = append(os.Environ(), "OKTETO_ENABLE_MANIFEST_V2=true")
+	cmd.Dir = repoDir
+	o, err := cmd.CombinedOutput()
+	if err != nil {
+		return "", fmt.Errorf("running command: \nerror: %s \noutput: %s", err.Error(), string(o))
+	}
+	return string(o), nil
 }
 
-func expectEnvSetting(output, namespace, repoDir, sha string) bool {
+func runOktetoDeployForceBuild(oktetoPath, repoDir string) (string, error) {
+	cmd := exec.Command(oktetoPath, "deploy", "--build", "-l", "debug")
+	cmd.Env = append(os.Environ(), "OKTETO_ENABLE_MANIFEST_V2=true")
+	cmd.Dir = repoDir
+	o, err := cmd.CombinedOutput()
+	if err != nil {
+		return string(o), fmt.Errorf("running command: \nerror: %s \noutput: %s", err.Error(), string(o))
+	}
+	return string(o), nil
+}
+
+func expectBuiltImageNotFound(output string) error {
+	if ok := strings.Contains(output, "image not found, building image"); !ok {
+		return fmt.Errorf("expected image not found, building image")
+	}
+	return nil
+}
+
+func expectImageFoundSkippingBuild(output string) error {
+	if ok := strings.Contains(output, "image found, skipping build"); !ok {
+		return fmt.Errorf("expected image found, skipping build")
+	}
+	return nil
+}
+
+func expectForceBuild(output string) error {
+	if ok := strings.Contains(output, "force build from manifest definition"); !ok {
+		return fmt.Errorf("expected force build from manifest definition")
+	}
+	return nil
+}
+
+func expectEnvSetting(output, namespace, repoDir, sha string) error {
 	rgty := okteto.Context().Registry
 	repo := fmt.Sprintf("%s/%s-app", namespace, repoDir)
 	img := fmt.Sprintf("%s/%s/%s-app@%s", okteto.Context().Registry, namespace, repoDir, sha)
-	tag := sha
-	return strings.Contains(output, fmt.Sprintf("envs registry=%s repository=%s image=%s tag=%s", rgty, repo, img, tag)) &&
-		strings.Contains(output, "manifest env vars set")
+
+	if ok := strings.Contains(output, fmt.Sprintf("envs registry=%s repository=%s image=%s tag=%s", rgty, repo, img, sha)) &&
+		strings.Contains(output, "manifest env vars set"); !ok {
+		return fmt.Errorf("expected envs registry=%s repository=%s image=%s tag=%s", rgty, repo, img, sha)
+	}
+	return nil
 }
 
-func expectHelmInstallation(output, releaseName, namespace string) bool {
-	return strings.Contains(output, fmt.Sprintf(`Release "%s" does not exist. Installing it now.`, releaseName)) &&
+func expectHelmInstallation(output, releaseName, namespace string) error {
+	if ok := strings.Contains(output, fmt.Sprintf(`Release "%s" does not exist. Installing it now.`, releaseName)) &&
 		strings.Contains(output, fmt.Sprintf("NAME: %s", releaseName)) &&
 		strings.Contains(output, fmt.Sprintf("NAMESPACE: %s", namespace)) &&
 		strings.Contains(output, "STATUS: deployed") &&
-		strings.Contains(output, "REVISION: 1")
+		strings.Contains(output, "REVISION: 1"); !ok {
+		return fmt.Errorf("expected helm chart to be installed")
+	}
+	return nil
 }
 
-func expectAppToBeRunning(releaseName, namespace string) bool {
+func expectHelmUpgrade(output, releaseName, namespace, revision string) error {
+	if ok := strings.Contains(output, fmt.Sprintf(`Release "%s" has been upgraded.`, releaseName)) &&
+		strings.Contains(output, fmt.Sprintf("NAME: %s", releaseName)) &&
+		strings.Contains(output, fmt.Sprintf("NAMESPACE: %s", namespace)) &&
+		strings.Contains(output, "STATUS: deployed") &&
+		strings.Contains(output, fmt.Sprintf("REVISION: %s", revision)); !ok {
+		return fmt.Errorf("expected helm chart to be upgraded to revision %s", revision)
+	}
+	return nil
+}
+
+func expectAppToBeRunning(releaseName, namespace string) error {
 	endpoint := fmt.Sprintf("https://%s-%s.%s", releaseName, namespace, appsSubdomain)
 	content, err := getContent(endpoint, 150, nil)
 	if err != nil {
-		log.Printf("error getting app content: %s", err.Error())
-		return false
+		return err
 	}
-	return strings.Contains(content, "Hello world!")
+	if ok := strings.Contains(content, "Hello world!"); !ok {
+		return fmt.Errorf("expected app content")
+	}
+	return nil
 }
 
 func writeFile(path, filename, content string) error {
