2f1e97818b837e665dc506f7710e91d616f44df2
Javier López Barba <javier@okteto.com>
2022-03-11 13:24:09 +0100

    feat: set vars as options as env vars (#2342)
    * feat: set vars as options as env vars
    
    Signed-off-by: Javier López Barba <javier@okteto.com>
    
    * refactor: split function in validate and set
    
    Signed-off-by: Javier López Barba <javier@okteto.com>

---
+++ cmd/deploy/validate.go
@@ -0,0 +1,43 @@
+// Copyright 2022 The Okteto Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package deploy
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func validateOptionVars(variables []string) error {
+	for _, v := range variables {
+		kv := strings.SplitN(v, "=", 2)
+		if len(kv) != 2 {
+			return fmt.Errorf("invalid variable value '%s': must follow KEY=VALUE format", v)
+		}
+		if err := os.Setenv(kv[0], kv[1]); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func setOptionVarsAsEnvs(variables []string) error {
+	for _, v := range variables {
+		kv := strings.SplitN(v, "=", 2)
+		if err := os.Setenv(kv[0], kv[1]); err != nil {
+			return err
+		}
+	}
+	return nil
+}
---
+++ cmd/deploy/validate_test.go
@@ -0,0 +1,51 @@
+// Copyright 2022 The Okteto Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package deploy
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_ValidateVars(t *testing.T) {
+
+	var tests = []struct {
+		name        string
+		variables   []string
+		expectedErr bool
+	}{
+		{
+			name:        "correct assingnament",
+			variables:   []string{"NAME=test"},
+			expectedErr: false,
+		},
+		{
+			name:        "bas assingnament",
+			variables:   []string{"NAME:test"},
+			expectedErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := validateOptionVars(tt.variables)
+			if tt.expectedErr {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+		})
+	}
+}
