e44a94a69563d970c8e5709689fdf76089b8188c
Pablo Chico de Guzman <pchico83@gmail.com>
2022-03-14 21:42:29 +0100

    Retry build errors if i/o timeout (#2351)
    * Retry build errors if i/o timeout
    
    Signed-off-by: Pablo Chico de Guzman <pchico83@gmail.com>
    
    * Retry "no such host" errors
    
    Signed-off-by: Pablo Chico de Guzman <pchico83@gmail.com>

--- pkg/registry/errors.go
+++ pkg/registry/errors.go
@@ -60,6 +60,10 @@ func IsTransientError(err error) bool {
 	case strings.Contains(err.Error(), "failed commit on ref") && strings.Contains(err.Error(), "500 Internal Server Error"),
 		strings.Contains(err.Error(), "transport is closing"):
 		return true
+	case strings.Contains(err.Error(), "transport: error while dialing: dial tcp: i/o timeout"):
+		return true
+	case strings.Contains(err.Error(), "error while dialing: dial tcp: lookup buildkit") && strings.Contains(err.Error(), "no such host"):
+		return true
 	case strings.Contains(err.Error(), "failed commit on ref") && strings.Contains(err.Error(), "400 Bad Request"):
 		return true
 	case strings.Contains(err.Error(), "failed to do request") && strings.Contains(err.Error(), "http: server closed idle connection"):
