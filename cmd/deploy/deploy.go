8743be29472638c169fac4d52ff49d09d9725b09
Javier López Barba <javier@okteto.com>
2022-03-25 13:01:57 +0100

    fix: save repository from up or init deploy
    Signed-off-by: Javier López Barba <javier@okteto.com>

--- cmd/deploy/deploy.go
+++ cmd/deploy/deploy.go
@@ -152,12 +152,6 @@ func Deploy(ctx context.Context) *cobra.Command {
 				}
 			}
 
-			cwd, err := os.Getwd()
-			if err != nil {
-				return fmt.Errorf("failed to get the current working directory: %w", err)
-			}
-
-			addEnvVars(ctx, cwd)
 			options.ShowCTA = oktetoLog.IsInteractive()
 			options.servicesToDeploy = args
 
@@ -231,6 +225,10 @@ func (dc *DeployCommand) RunDeploy(ctx context.Context, deployOptions *Options) 
 	cwd, err := os.Getwd()
 	if err != nil {
 		return fmt.Errorf("failed to get the current working directory: %w", err)
+	}
+
+	if err := addEnvVars(ctx, cwd); err != nil {
+		return err
 	}
 	oktetoLog.Debugf("creating temporal kubeconfig file '%s'", dc.TempKubeconfigFile)
 	if err := dc.Kubeconfig.Modify(dc.Proxy.GetPort(), dc.Proxy.GetToken(), dc.TempKubeconfigFile); err != nil {
