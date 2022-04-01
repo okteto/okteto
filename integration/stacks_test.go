6bc72345423fef08cfdf074306d762de8bfcd552
Javier López Barba <javier@okteto.com>
2022-04-01 14:36:33 +0200

    fix: change to new log-level flag (#2476)
    Signed-off-by: Javier López Barba <javier@okteto.com>

--- integration/stacks_test.go
+++ integration/stacks_test.go
@@ -106,9 +106,9 @@ func TestStacks(t *testing.T) {
 
 func deployStack(ctx context.Context, oktetoPath, stackPath, dir string) error {
 	log.Printf("okteto stack deploy %s", stackPath)
-	cmd := exec.Command(oktetoPath, "deploy", "-f", stackPath, "--build", "--wait", "--loglevel=info")
+	cmd := exec.Command(oktetoPath, "deploy", "-f", stackPath, "--build", "--wait", "--log-level=info")
 	if runtime.GOOS == "windows" {
-		cmd = exec.Command(oktetoPath, "stack", "deploy", "-f", stackPath, "--build", "--wait", "--loglevel=info")
+		cmd = exec.Command(oktetoPath, "stack", "deploy", "-f", stackPath, "--build", "--wait", "--log-level=info")
 	}
 	cmd.Env = os.Environ()
 	cmd.Dir = dir
@@ -130,7 +130,7 @@ func deployStack(ctx context.Context, oktetoPath, stackPath, dir string) error {
 
 func destroyStack(ctx context.Context, oktetoPath, stackManifest, dir string) error {
 	log.Printf("okteto stack destroy")
-	cmd := exec.Command(oktetoPath, "stack", "destroy", "-f", stackManifest, "--loglevel=debug")
+	cmd := exec.Command(oktetoPath, "stack", "destroy", "-f", stackManifest, "--log-level=debug")
 	cmd.Env = os.Environ()
 	cmd.Dir = dir
 	o, err := cmd.CombinedOutput()
