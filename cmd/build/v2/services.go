4a0ea549b8a4687cf3d78e6873e0867dff1dbcbb
Javier López Barba <javier@okteto.com>
2022-04-27 13:24:34 +0200

    fix: exapnd env vars
    Signed-off-by: Javier López Barba <javier@okteto.com>

--- cmd/build/v2/services.go
+++ cmd/build/v2/services.go
@@ -49,6 +49,9 @@ func (bc *OktetoBuilder) GetServicesToBuild(ctx context.Context, manifest *model
 
 	if len(toBuild) == 0 {
 		oktetoLog.Information("Images were already built. To rebuild your images run 'okteto build' or 'okteto deploy --build'")
+		if err := manifest.ExpandEnvVars(); err != nil {
+			return nil, err
+		}
 		return nil, nil
 	}
 
