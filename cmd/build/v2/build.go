ee6eda446f485d7fcd9c95533550bcfa44d35311
Teresa Romero <teresa@okteto.com>
2022-04-28 09:00:31 +0200

    refactor: change debug log
    Signed-off-by: Teresa Romero <teresa@okteto.com>

--- cmd/build/v2/build.go
+++ cmd/build/v2/build.go
@@ -216,12 +216,12 @@ func (bc *OktetoBuilder) optimizeBuild(buildOptions *types.BuildOptions, svcName
 	oktetoLog.Debug("found optimizing the build flow")
 	globalReference := strings.Replace(buildOptions.Tag, okteto.DevRegistry, okteto.GlobalRegistry, 1)
 	if _, err := bc.Registry.GetImageTagWithDigest(globalReference); err == nil {
-		oktetoLog.Debugf("Skipping '%s' build. Image already exists at the Okteto Registry", svcName)
+		oktetoLog.Debugf("Skipping '%s' build. Image already exists at the Okteto Global Registry: %s", svcName, globalReference)
 		return globalReference, nil
 	}
 	if registry.IsDevRegistry(buildOptions.Tag) {
 		if _, err := bc.Registry.GetImageTagWithDigest(buildOptions.Tag); err == nil {
-			oktetoLog.Debugf("skipping build: image %s is already built", buildOptions.Tag)
+			oktetoLog.Debugf("Skipping '%s' build: Image already exists at the Okteto Registry: %s", svcName, buildOptions.Tag)
 			return buildOptions.Tag, nil
 		}
 	}
