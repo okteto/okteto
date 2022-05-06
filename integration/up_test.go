ff2bcd93beccd2e63796c2a20b21beb6de6b0aca
Javier López Barba <javier@okteto.com>
2022-05-06 14:27:31 +0200

    fix: compose test e2e (#2654)
    * fix: compose test e2e
    
    Signed-off-by: Javier López Barba <javier@okteto.com>
    
    * fix: test
    
    Signed-off-by: Javier López Barba <javier@okteto.com>
    
    * fix: compose test
    
    Signed-off-by: Javier López Barba <javier@okteto.com>
    
    * run: all test
    
    Signed-off-by: Javier López Barba <javier@okteto.com>

--- integration/up_test.go
+++ integration/up_test.go
@@ -734,11 +734,11 @@ func TestUpCompose(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	if len(svc.Spec.Ports) != 1 {
-		t.Fatalf("Expected to have only one endpoint for svc 'vote' but got %d", len(svc.Spec.Ports))
+	if len(svc.Spec.Ports) != 2 {
+		t.Fatalf("Expected to have two ports for endpoints for svc 'vote' but got %d", len(svc.Spec.Ports))
 	}
 
-	ingress, err := getIngress(ctx, namespace, "okteto-vote")
+	ingress, err := getIngress(ctx, namespace, "vote")
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -753,9 +753,6 @@ func TestUpCompose(t *testing.T) {
 				t.Fatal("Didn't expect a debugger to have an endpoint")
 			}
 		}
-	}
-	if len(svc.Spec.Ports) != 1 {
-		t.Fatalf("Expected to have only one endpoint for svc 'vote' but got %d", len(svc.Spec.Ports))
 	}
 
 	port := "5005"
