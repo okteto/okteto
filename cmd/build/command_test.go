76d887bca3ecaa8c8459a66494de5a57756000da
adripedriza <adripedriza@gmail.com>
2022-05-24 16:00:50 +0200

    feat: add t.Parallel() in test
    Signed-off-by: adripedriza <adripedriza@gmail.com>

--- cmd/build/command_test.go
+++ cmd/build/command_test.go
@@ -120,6 +120,7 @@ func TestIsBuildV2(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
 			answer := isBuildV2(tt.manifest)
 			assert.Equal(t, answer, tt.expectedAnswer)
 		})
