aa7e1ca882216990a18c5c25b0325a3f5baa554e
adripedriza <adripedriza@gmail.com>
2022-06-29 13:28:51 +0200

    remove dead code
    Signed-off-by: adripedriza <adripedriza@gmail.com>

--- cmd/up/forwards_test.go
+++ cmd/up/forwards_test.go
@@ -98,7 +98,6 @@ func TestGlobalForwarderAddsProperlyPortsToForward(t *testing.T) {
 	}
 
 	for _, tt := range tests {
-		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
 			err := addGlobalForwards(tt.upContext)
 			if !tt.expectedErr {
