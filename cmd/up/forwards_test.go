7f66bb7f3811d246f61e74c1863f9a48849e33d0
adripedriza <adripedriza@gmail.com>
2022-06-29 15:34:57 +0200

    fix: remove parallel test when writing in map (#2844)
    * fix: remove parallel test when writing in map
    
    Signed-off-by: adripedriza <adripedriza@gmail.com>
    
    * remove dead code
    
    Signed-off-by: adripedriza <adripedriza@gmail.com>

--- cmd/up/forwards_test.go
+++ cmd/up/forwards_test.go
@@ -98,9 +98,7 @@ func TestGlobalForwarderAddsProperlyPortsToForward(t *testing.T) {
 	}
 
 	for _, tt := range tests {
-		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
-			t.Parallel()
 			err := addGlobalForwards(tt.upContext)
 			if !tt.expectedErr {
 				assert.Nil(t, err)
