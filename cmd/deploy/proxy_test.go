0d2284c6e466a7171999667a7b67b1e99ef7a46e
adripedriza <adripedriza@gmail.com>
2022-07-07 11:00:58 +0200

    add test
    Signed-off-by: adripedriza <adripedriza@gmail.com>

---
+++ cmd/deploy/proxy_test.go
@@ -0,0 +1,60 @@
+package deploy
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var (
+	ph = &proxyHandler{}
+)
+
+func Test_TranslateInvalidResourceBody(t *testing.T) {
+	var tests = []struct {
+		name string
+		body []byte
+	}{
+		{
+			name: "null body json.RawMessage",
+			body: []byte(``),
+		},
+		{
+			name: "correct body json.RawMessage",
+			body: []byte(`{"kind":"Secret","apiVersion":"v1","metadata":{"name":"sh.helm.release.v1.movies.v6"},"type":"helm.sh/release.v1"}`),
+		},
+		{
+			name: "incorrect body typemeta",
+			body: []byte(`{"kind": {"key": "value"},"apiVersion":"v1","metadata":{"name":"sh.helm.release.v1.movies.v6"},"type":"helm.sh/release.v1"}`),
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			_, err := ph.translateBody(tt.body)
+			assert.NoError(t, err)
+		})
+	}
+}
+
+func Test_TranslateInvalidResourceSpec(t *testing.T) {
+	invalidResourceSpec := map[string]json.RawMessage{
+		"spec": []byte(`{"selector": "invalid value"}`),
+	}
+	assert.NoError(t, ph.translateDeploymentSpec(invalidResourceSpec))
+	assert.NoError(t, ph.translateStatefulSetSpec(invalidResourceSpec))
+	assert.NoError(t, ph.translateReplicationControllerSpec(invalidResourceSpec))
+	assert.NoError(t, ph.translateReplicaSetSpec(invalidResourceSpec))
+	assert.NoError(t, ph.translateDaemonSetSpec(invalidResourceSpec))
+
+	assert.NoError(t, ph.translateJobSpec(map[string]json.RawMessage{
+		"spec": []byte(`{"parallelism": "invalid value"}`),
+	}))
+
+	assert.NoError(t, ph.translateCronJobSpec(map[string]json.RawMessage{
+		"spec": []byte(`{"schedule": 1}`),
+	}))
+}
