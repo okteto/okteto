41a0e3adf8161e4ca236974c25aaddb44013a56c
Javier López Barba <javier@okteto.com>
2022-07-13 12:59:16 +0200

    refactor: encapsulate into a function get token
    Signed-off-by: Javier López Barba <javier@okteto.com>

--- integration/up/up_test.go
+++ integration/up/up_test.go
@@ -31,7 +31,6 @@ import (
 	"github.com/okteto/okteto/integration"
 	"github.com/okteto/okteto/integration/commands"
 	"github.com/okteto/okteto/pkg/model"
-	"github.com/okteto/okteto/pkg/okteto"
 )
 
 var (
@@ -68,11 +67,7 @@ func TestMain(m *testing.M) {
 		log.Printf("kubectl is not in the path: %s", err)
 		os.Exit(1)
 	}
-	if v := os.Getenv(model.OktetoTokenEnvVar); v != "" {
-		token = v
-	} else {
-		token = okteto.Context().Token
-	}
+	token = integration.GetToken()
 
 	exitCode := m.Run()
 	os.Exit(exitCode)
