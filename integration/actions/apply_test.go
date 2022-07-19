44e67b6a96ac1491f0d4677a7e774f291d5a9f13
Javier López Barba <javier@okteto.com>
2022-07-19 11:25:01 +0200

    fix: e2e test
    Signed-off-by: Javier López Barba <javier@okteto.com>

--- integration/actions/apply_test.go
+++ integration/actions/apply_test.go
@@ -31,6 +31,7 @@ import (
 	"github.com/okteto/okteto/integration/commands"
 	"github.com/okteto/okteto/pkg/config"
 	"github.com/okteto/okteto/pkg/k8s/kubeconfig"
+	"github.com/okteto/okteto/pkg/model"
 	"github.com/okteto/okteto/pkg/okteto"
 	"github.com/stretchr/testify/assert"
 )
@@ -75,6 +76,7 @@ func TestApplyPipeline(t *testing.T) {
 	namespace := getTestNamespace()
 	oktetoPath, err := integration.GetOktetoPath()
 	assert.NoError(t, err)
+	t.Setenv(model.OktetoTokenEnvVar, okteto.Context().Token)
 
 	assert.NoError(t, executeCreateNamespaceAction(namespace))
 	assert.NoError(t, commands.RunOktetoKubeconfig(oktetoPath, ""))
