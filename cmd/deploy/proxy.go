e4461e7d6d946ab4e0d63d61490de53197842eb3
Javier López Barba <javier@okteto.com>
2022-08-01 15:24:26 +0200

    fix: unit tests
    Signed-off-by: Javier López Barba <javier@okteto.com>

--- cmd/deploy/proxy.go
+++ cmd/deploy/proxy.go
@@ -218,6 +218,7 @@ func (ph *proxyHandler) getProxyHandler(token string, clusterConfig *rest.Config
 
 		// Modify all resources updated or created to include the label.
 		if r.Method == "PUT" || r.Method == "POST" {
+			isCreation := r.Method == "POST"
 			b, err := io.ReadAll(r.Body)
 			if err != nil {
 				oktetoLog.Infof("could not read the request body: %s", err)
@@ -230,7 +231,7 @@ func (ph *proxyHandler) getProxyHandler(token string, clusterConfig *rest.Config
 				return
 			}
 
-			b, err = ph.translateBody(b, r.Method)
+			b, err = ph.translateBody(b, isCreation)
 			if err != nil {
 				oktetoLog.Info(err)
 				rw.WriteHeader(500)
@@ -258,7 +259,7 @@ func (ph *proxyHandler) SetDivert(divertedNamespace string) {
 	ph.DivertedNamespace = divertedNamespace
 }
 
-func (ph *proxyHandler) translateBody(b []byte, method string) ([]byte, error) {
+func (ph *proxyHandler) translateBody(b []byte, isCreation bool) ([]byte, error) {
 	var body map[string]json.RawMessage
 	if err := json.Unmarshal(b, &body); err != nil {
 		oktetoLog.Infof("error unmarshalling resource body on proxy: %s", err.Error())
@@ -281,7 +282,7 @@ func (ph *proxyHandler) translateBody(b []byte, method string) ([]byte, error) {
 			return nil, err
 		}
 	case "StatefulSet":
-		if err := ph.translateStatefulSetSpec(body, method); err != nil {
+		if err := ph.translateStatefulSetSpec(body, isCreation); err != nil {
 			return nil, err
 		}
 	case "Job":
@@ -356,14 +357,14 @@ func (ph *proxyHandler) translateDeploymentSpec(body map[string]json.RawMessage)
 	return nil
 }
 
-func (ph *proxyHandler) translateStatefulSetSpec(body map[string]json.RawMessage, method string) error {
+func (ph *proxyHandler) translateStatefulSetSpec(body map[string]json.RawMessage, isCreation bool) error {
 	var spec appsv1.StatefulSetSpec
 	if err := json.Unmarshal(body["spec"], &spec); err != nil {
 		oktetoLog.Infof("error unmarshalling statefulset spec on proxy: %s", err.Error())
 		return nil
 	}
 	labels.SetInMetadata(&spec.Template.ObjectMeta, model.DeployedByLabel, ph.Name)
-	if method == "POST" {
+	if isCreation {
 		for _, pvc := range spec.VolumeClaimTemplates {
 			labels.SetInMetadata(&pvc.ObjectMeta, model.DeployedByLabel, ph.Name)
 		}
