5a232244d48f9484f8c43dd5d132f02e1bffffef
Javier López Barba <javier@okteto.com>
2022-08-02 14:22:15 +0200

    fix: volumes not being deleted (#2955)
    * fix: volumes not being deleted
    
    Signed-off-by: Javier López Barba <javier@okteto.com>
    
    * fix: remoe unused function
    
    Signed-off-by: Javier López Barba <javier@okteto.com>
    
    * fix: add labels only on creation
    
    Signed-off-by: Javier López Barba <javier@okteto.com>
    
    * fix: unit tests
    
    Signed-off-by: Javier López Barba <javier@okteto.com>
    
    * fix: deep source copy of large struct
    
    Signed-off-by: Javier López Barba <javier@okteto.com>
    
    * tests: add coverage for different methods
    
    Signed-off-by: Javier López Barba <javier@okteto.com>

--- cmd/deploy/proxy.go
+++ cmd/deploy/proxy.go
@@ -227,6 +227,7 @@ func (ph *proxyHandler) getProxyHandler(token string, clusterConfig *rest.Config
 
 		// Modify all resources updated or created to include the label.
 		if r.Method == "PUT" || r.Method == "POST" {
+			isCreation := r.Method == "POST"
 			b, err := io.ReadAll(r.Body)
 			if err != nil {
 				oktetoLog.Infof("could not read the request body: %s", err)
@@ -239,7 +240,7 @@ func (ph *proxyHandler) getProxyHandler(token string, clusterConfig *rest.Config
 				return
 			}
 
-			b, err = ph.translateBody(b)
+			b, err = ph.translateBody(b, isCreation)
 			if err != nil {
 				oktetoLog.Info(err)
 				rw.WriteHeader(500)
@@ -267,7 +268,7 @@ func (ph *proxyHandler) SetDivert(divertedNamespace string) {
 	ph.DivertedNamespace = divertedNamespace
 }
 
-func (ph *proxyHandler) translateBody(b []byte) ([]byte, error) {
+func (ph *proxyHandler) translateBody(b []byte, isCreation bool) ([]byte, error) {
 	var body map[string]json.RawMessage
 	if err := json.Unmarshal(b, &body); err != nil {
 		oktetoLog.Infof("error unmarshalling resource body on proxy: %s", err.Error())
@@ -290,7 +291,7 @@ func (ph *proxyHandler) translateBody(b []byte) ([]byte, error) {
 			return nil, err
 		}
 	case "StatefulSet":
-		if err := ph.translateStatefulSetSpec(body); err != nil {
+		if err := ph.translateStatefulSetSpec(body, isCreation); err != nil {
 			return nil, err
 		}
 	case "Job":
@@ -365,13 +366,18 @@ func (ph *proxyHandler) translateDeploymentSpec(body map[string]json.RawMessage)
 	return nil
 }
 
-func (ph *proxyHandler) translateStatefulSetSpec(body map[string]json.RawMessage) error {
+func (ph *proxyHandler) translateStatefulSetSpec(body map[string]json.RawMessage, isCreation bool) error {
 	var spec appsv1.StatefulSetSpec
 	if err := json.Unmarshal(body["spec"], &spec); err != nil {
 		oktetoLog.Infof("error unmarshalling statefulset spec on proxy: %s", err.Error())
 		return nil
 	}
 	labels.SetInMetadata(&spec.Template.ObjectMeta, model.DeployedByLabel, ph.Name)
+	if isCreation {
+		for idx := range spec.VolumeClaimTemplates {
+			labels.SetInMetadata(&spec.VolumeClaimTemplates[idx].ObjectMeta, model.DeployedByLabel, ph.Name)
+		}
+	}
 	ph.applyDivert(&spec.Template.Spec)
 	specAsByte, err := json.Marshal(spec)
 	if err != nil {
--- cmd/deploy/proxy_test.go
+++ cmd/deploy/proxy_test.go
@@ -13,20 +13,39 @@ var (
 
 func Test_TranslateInvalidResourceBody(t *testing.T) {
 	var tests = []struct {
-		name string
-		body []byte
+		name       string
+		body       []byte
+		isCreation bool
 	}{
 		{
-			name: "null body json.RawMessage",
-			body: []byte(``),
+			name:       "null body json.RawMessage POST",
+			body:       []byte(``),
+			isCreation: true,
 		},
 		{
-			name: "correct body json.RawMessage",
-			body: []byte(`{"kind":"Secret","apiVersion":"v1","metadata":{"name":"sh.helm.release.v1.movies.v6"},"type":"helm.sh/release.v1"}`),
+			name:       "correct body json.RawMessage POST",
+			body:       []byte(`{"kind":"Secret","apiVersion":"v1","metadata":{"name":"sh.helm.release.v1.movies.v6"},"type":"helm.sh/release.v1"}`),
+			isCreation: true,
 		},
 		{
-			name: "incorrect body typemeta",
-			body: []byte(`{"kind": {"key": "value"},"apiVersion":"v1","metadata":{"name":"sh.helm.release.v1.movies.v6"},"type":"helm.sh/release.v1"}`),
+			name:       "incorrect body typemeta POST",
+			body:       []byte(`{"kind": {"key": "value"},"apiVersion":"v1","metadata":{"name":"sh.helm.release.v1.movies.v6"},"type":"helm.sh/release.v1"}`),
+			isCreation: true,
+		},
+		{
+			name:       "null body json.RawMessage PUT",
+			body:       []byte(``),
+			isCreation: false,
+		},
+		{
+			name:       "correct body json.RawMessage PUT",
+			body:       []byte(`{"kind":"Secret","apiVersion":"v1","metadata":{"name":"sh.helm.release.v1.movies.v6"},"type":"helm.sh/release.v1"}`),
+			isCreation: false,
+		},
+		{
+			name:       "incorrect body typemeta PUT",
+			body:       []byte(`{"kind": {"key": "value"},"apiVersion":"v1","metadata":{"name":"sh.helm.release.v1.movies.v6"},"type":"helm.sh/release.v1"}`),
+			isCreation: false,
 		},
 	}
 
@@ -34,7 +53,7 @@ func Test_TranslateInvalidResourceBody(t *testing.T) {
 		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
-			_, err := ph.translateBody(tt.body)
+			_, err := ph.translateBody(tt.body, tt.isCreation)
 			assert.NoError(t, err)
 		})
 	}
@@ -45,7 +64,7 @@ func Test_TranslateInvalidResourceSpec(t *testing.T) {
 		"spec": []byte(`{"selector": "invalid value"}`),
 	}
 	assert.NoError(t, ph.translateDeploymentSpec(invalidResourceSpec))
-	assert.NoError(t, ph.translateStatefulSetSpec(invalidResourceSpec))
+	assert.NoError(t, ph.translateStatefulSetSpec(invalidResourceSpec, true))
 	assert.NoError(t, ph.translateReplicationControllerSpec(invalidResourceSpec))
 	assert.NoError(t, ph.translateReplicaSetSpec(invalidResourceSpec))
 	assert.NoError(t, ph.translateDaemonSetSpec(invalidResourceSpec))
