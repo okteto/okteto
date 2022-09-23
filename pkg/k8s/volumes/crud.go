10b27534d43a8909355e36b53cb70bf29d70034d
Agustín Díaz <49416765+AgustinRamiroDiaz@users.noreply.github.com>
2022-09-23 08:06:05 -0300

    Fix formatting
    Co-authored-by: Tere <teresa@okteto.com>

--- pkg/k8s/volumes/crud.go
+++ pkg/k8s/volumes/crud.go
@@ -74,7 +74,7 @@ func CreateForDev(ctx context.Context, dev *model.Dev, c kubernetes.Interface, d
 			if !isDynamicallyProvisionedPVCError(err, pvc.Name) {
 				return fmt.Errorf("error updating kubernetes volume claim: %w", err)
 			}
-			oktetoLog.Debug("could not update pvc in namespace: %w", dev.Namespace, err)
+			oktetoLog.Debug("could not update pvc in namespace %s: %w", dev.Namespace, err)
 			oktetoLog.Warning("Could not increase the size of the dev volume from %s to %s: try running 'okteto down -v' and 'okteto up', or talk to your administrator (the PVC's storage class must support 'allowVolumeExpansion' to be able to upscale dev volumes).", k8Volume.Spec.Resources.Requests[apiv1.ResourceStorage], pvc.Spec.Resources.Requests[apiv1.ResourceStorage])
 		}
 	}
