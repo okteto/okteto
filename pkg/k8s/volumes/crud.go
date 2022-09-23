ad14c654cecda20789ae81feb6d1bb76314f6f28
Agustin Ramiro Diaz <agustin.ramiro.diaz@gmail.com>
2022-09-23 08:13:23 -0300

    Split warning into multiple lines
    Signed-off-by: Agustin Ramiro Diaz <agustin.ramiro.diaz@gmail.com>

--- pkg/k8s/volumes/crud.go
+++ pkg/k8s/volumes/crud.go
@@ -75,7 +75,10 @@ func CreateForDev(ctx context.Context, dev *model.Dev, c kubernetes.Interface, d
 				return fmt.Errorf("error updating kubernetes volume claim: %w", err)
 			}
 			oktetoLog.Debug("could not update pvc in namespace %s: %w", dev.Namespace, err)
-			oktetoLog.Warning("Could not increase the size of the dev volume from %s to %s: try running 'okteto down -v' and 'okteto up', or talk to your administrator (the PVC's storage class must support 'allowVolumeExpansion' to be able to upscale dev volumes).", k8Volume.Spec.Resources.Requests[apiv1.ResourceStorage], pvc.Spec.Resources.Requests[apiv1.ResourceStorage])
+			oktetoLog.Warning(`Could not increase the size of the dev volume from %s to %s: 
+try running 'okteto down -v' and 'okteto up', or talk to your administrator 
+(the PVC's storage class must support 'allowVolumeExpansion' to be able to upscale dev volumes).`,
+				k8Volume.Spec.Resources.Requests[apiv1.ResourceStorage], pvc.Spec.Resources.Requests[apiv1.ResourceStorage])
 		}
 	}
 	return nil
