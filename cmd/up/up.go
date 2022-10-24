8275edf61cd5fcae99131508e60a859fda879310
Adel Salakh <adel.salakh@gmail.com>
2022-10-24 17:43:58 +0200

    Fix error logging crash on context load failure
    Signed-off-by: Adel Salakh <adel@zetico.io>

--- cmd/up/up.go
+++ cmd/up/up.go
@@ -227,7 +227,7 @@ func Up() *cobra.Command {
 			}
 			up.Client, up.RestConfig, err = okteto.GetK8sClient()
 			if err != nil {
-				return fmt.Errorf("failed to load okteto context '%s': %v", up.Dev.Context, err)
+				return fmt.Errorf("failed to load okteto context: %v", err)
 			}
 
 			// if manifest v1 - either set autocreate: true or pass --deploy (okteto forces autocreate: true)
