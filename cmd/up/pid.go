e0f4c33d4ddaff971b70f00b3e655c1e7cdc44c1
Javier López Barba <javier@okteto.com>
2022-11-03 16:06:36 +0100

    refactor: remove unused code
    Signed-off-by: Javier López Barba <javier@okteto.com>

--- cmd/up/pid.go
+++ cmd/up/pid.go
@@ -96,29 +96,3 @@ func (pc pidController) delete() {
 func (pp osPIDProvider) provide() int {
 	return os.Getpid()
 }
-
-// createPIDFile creates a PID file to track Up state and existence
-func createPIDFile(ns, dpName string) error {
-	filePath := filepath.Join(config.GetAppHome(ns, dpName), "okteto.pid")
-	file, err := os.Create(filePath)
-	if err != nil {
-		return fmt.Errorf("unable to create PID file at %s", filePath)
-	}
-	defer func() {
-		if err := file.Close(); err != nil {
-			oktetoLog.Debugf("Error closing file %s: %s", filePath, err)
-		}
-	}()
-	if _, err := file.WriteString(strconv.Itoa(os.Getpid())); err != nil {
-		return fmt.Errorf("unable to write to PID file at %s", filePath)
-	}
-	return nil
-}
-
-// cleanPIDFile deletes PID file after Up finishes
-func cleanPIDFile(ns, dpName string) {
-	filePath := filepath.Join(config.GetAppHome(ns, dpName), "okteto.pid")
-	if err := os.Remove(filePath); err != nil && !os.IsNotExist(err) {
-		oktetoLog.Infof("unable to delete PID file at %s", filePath)
-	}
-}
