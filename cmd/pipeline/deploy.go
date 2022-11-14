a9a8389b05166bd1b600b5263719ac998e846489
Ignacio Fuertes <nacho@okteto.com>
2022-11-14 11:07:09 +0100

    Added a warning message to display to the user indicating that the streaming logs cannot be retrieved for connectivity issues
    Signed-off-by: Ignacio Fuertes <nacho@okteto.com>

--- cmd/pipeline/deploy.go
+++ cmd/pipeline/deploy.go
@@ -250,6 +250,7 @@ func (pc *Command) waitUntilRunning(ctx context.Context, name string, action *ty
 		defer wg.Done()
 		err := pc.streamPipelineLogs(waitCtx, name, action.Name)
 		if err != nil {
+			oktetoLog.Warning("pipeline logs cannot be streamed due to connectivity issues")
 			oktetoLog.Infof("pipeline logs cannot be streamed due to connectivity issues: %v", err)
 		}
 	}(&wg)
