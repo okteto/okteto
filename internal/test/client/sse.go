3b80023c683c756637c754e3be9a3b15e4657951
Teresa Romero <teresa@okteto.com>
2022-11-28 12:07:08 +0100

    docs: fix desriptions
    Signed-off-by: Teresa Romero <teresa@okteto.com>

--- internal/test/client/sse.go
+++ internal/test/client/sse.go
@@ -4,23 +4,24 @@ import (
 	"context"
 )
 
-// FakeSSEClient mocks the previews interface
+// FakeSSEClient mocks the sse client interface
 type FakeSSEClient struct {
 	response *FakeSSEResponse
 }
 
+// FakeSSEResponse mocks the sse response
 type FakeSSEResponse struct {
 	StreamErr error
 }
 
-// NewFakeSSEClient returns a new fake preview client
+// NewFakeSSEClient returns a new fake sse client
 func NewFakeSSEClient(response *FakeSSEResponse) *FakeSSEClient {
 	return &FakeSSEClient{
 		response: response,
 	}
 }
 
-// List list namespaces
+// StreamPipelineLogs starts the streaming of pipeline logs
 func (c *FakeSSEClient) StreamPipelineLogs(ctx context.Context, _, _, _ string) error {
 	return c.response.StreamErr
 }
