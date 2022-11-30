e457d31ee87afef77d0e058a0518c98f7c0eda53
Javier López Barba <javier@okteto.com>
2022-11-30 11:32:57 +0100

    feat: add dependencies stage (#3251)
    * feat: add dependencies stage
    
    Signed-off-by: Javier López Barba <javier@okteto.com>
    
    * refactor: show dep name
    
    Signed-off-by: Javier López Barba <javier@okteto.com>
    
    * feat: add stage in the destroy command
    
    Signed-off-by: Javier López Barba <javier@okteto.com>
    
    Signed-off-by: Javier López Barba <javier@okteto.com>

--- cmd/deploy/deploy.go
+++ cmd/deploy/deploy.go
@@ -340,6 +340,7 @@ func (dc *DeployCommand) RunDeploy(ctx context.Context, deployOptions *Options) 
 	// TODO: take this out to a new function deploy dependencies
 	for depName, dep := range deployOptions.Manifest.Dependencies {
 		oktetoLog.Information("Deploying dependency '%s'", depName)
+		oktetoLog.SetStage(fmt.Sprintf("Deploying dependency %s", depName))
 		dep.Variables = append(dep.Variables, model.EnvVar{
 			Name:  "OKTETO_ORIGIN",
 			Value: "okteto-deploy",
@@ -366,6 +367,7 @@ func (dc *DeployCommand) RunDeploy(ctx context.Context, deployOptions *Options) 
 			return err
 		}
 	}
+	oktetoLog.SetStage("")
 
 	if err := buildImages(ctx, dc.Builder.Build, dc.Builder.GetServicesToBuild, deployOptions); err != nil {
 		return updateConfigMapStatusError(ctx, cfg, c, data, err)
