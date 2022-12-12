d652686e5dc67e63019fd4d16f01d068485efae9
Javier López Barba <javier@okteto.com>
2022-12-12 09:49:10 +0100

    fix: use notempty instead of notnil
    Signed-off-by: Javier López Barba <javier@okteto.com>

--- cmd/deploy/command_configuration_test.go
+++ cmd/deploy/command_configuration_test.go
@@ -96,7 +96,7 @@ devs:
 	assert.Equal(t, expectedCfg.Namespace, currentCfg.Namespace)
 	assert.Equal(t, expectedCfg.Labels, currentCfg.Labels)
 	assert.Equal(t, expectedCfg.Data, currentCfg.Data)
-	assert.NotNil(t, currentCfg.Annotations[model.LastUpdatedAnnotation])
+	assert.NotEmpty(t, currentCfg.Annotations[model.LastUpdatedAnnotation])
 }
 
 func Test_mergeServicesToDeployFromOptionsAndManifest(t *testing.T) {
