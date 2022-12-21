9a807634fb07c3a11ce8bbb9049400f1915191a6
Teresa Romero <teresa@okteto.com>
2022-12-21 15:37:56 +0100

    chore: remove unsued err
    Signed-off-by: Teresa Romero <teresa@okteto.com>

--- cmd/namespace/errors.go
+++ cmd/namespace/errors.go
@@ -6,6 +6,4 @@ import (
 
 var errFailedDeleteNamespace = errors.New("failed to delete namespace")
 
-var errNoStatusLabel = errors.New("namespace does not have label for status")
-
 var errDeleteNamespaceTimeout = errors.New("namespace delete didn't finish")
