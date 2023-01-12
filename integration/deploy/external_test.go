442872de7c4fbe95f5f2329b524b1797278009c7
adripedriza <adripedriza@gmail.com>
2023-01-12 12:24:40 +0100

    fix: add markdown reading
    Signed-off-by: adripedriza <adripedriza@gmail.com>

---
+++ integration/deploy/external_test.go
@@ -0,0 +1,115 @@
+//go:build integration
+// +build integration
+
+// Copyright 2023 The Okteto Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package deploy
+
+import (
+	"context"
+	b64 "encoding/base64"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/okteto/okteto/cmd/deploy"
+	"github.com/okteto/okteto/integration"
+	"github.com/okteto/okteto/integration/commands"
+	"github.com/okteto/okteto/pkg/okteto"
+	"github.com/stretchr/testify/require"
+)
+
+const (
+	oktetoManifestWithExternalContent = `
+deploy:
+  - name: test command
+    command: echo hola
+external:
+  test:
+    notes: readme.md
+    endpoints:
+    - name: test-endpoint
+      url: https://test.com
+`
+	notesContent = `## TEST
+# This is a test
+`
+)
+
+func Test_ExternalsFromOktetoManifestWithNotesContent(t *testing.T) {
+	ctx := context.Background()
+	t.Parallel()
+	oktetoPath, err := integration.GetOktetoPath()
+	require.NoError(t, err)
+
+	dir := t.TempDir()
+	require.NoError(t, createExternalNotes(dir))
+	require.NoError(t, createManifest(dir))
+
+	testNamespace := integration.GetTestNamespace("ExternalDeploy", user)
+	namespaceOpts := &commands.NamespaceOptions{
+		Namespace:  testNamespace,
+		OktetoHome: dir,
+		Token:      token,
+	}
+	require.NoError(t, commands.RunOktetoCreateNamespace(oktetoPath, namespaceOpts))
+	defer commands.RunOktetoDeleteNamespace(oktetoPath, namespaceOpts)
+	require.NoError(t, commands.RunOktetoKubeconfig(oktetoPath, dir))
+
+	deployOptions := &commands.DeployOptions{
+		Workdir:    dir,
+		Namespace:  testNamespace,
+		OktetoHome: dir,
+		Token:      token,
+	}
+	require.NoError(t, commands.RunOktetoDeploy(oktetoPath, deployOptions))
+
+	externalControl, err := deploy.GetExternalControl(okteto.NewK8sClientProvider(), filepath.Join(dir, ".kube", "config"))
+	require.NoError(t, err)
+
+	externals, err := externalControl.List(ctx, namespaceOpts.Namespace, "")
+
+	require.NoError(t, err)
+	require.Len(t, externals, 1)
+
+	decodedContent, err := b64.StdEncoding.DecodeString(externals[0].Notes.Markdown)
+	require.NoError(t, err)
+
+	require.Equal(t, string(decodedContent), notesContent)
+
+	destroyOptions := &commands.DestroyOptions{
+		Workdir:    dir,
+		Namespace:  testNamespace,
+		OktetoHome: dir,
+	}
+	require.NoError(t, commands.RunOktetoDestroy(oktetoPath, destroyOptions))
+}
+
+func createExternalNotes(dir string) error {
+	notesPath := filepath.Join(dir, "readme.md")
+	notesContent := []byte(notesContent)
+	if err := os.WriteFile(notesPath, notesContent, 0600); err != nil {
+		return err
+	}
+	return nil
+}
+
+func createManifest(dir string) error {
+	manifestPath := filepath.Join(dir, "okteto.yml")
+	manifestContent := []byte(oktetoManifestWithExternalContent)
+	if err := os.WriteFile(manifestPath, manifestContent, 0600); err != nil {
+		return err
+	}
+	return nil
+}
