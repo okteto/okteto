1e9d149afcdaa7d176b04f1287748cda1eeb3519
Javier López Barba <javier@okteto.com>
2023-01-13 16:58:19 +0100

    feat: remote fixes
    Signed-off-by: Javier López Barba <javier@okteto.com>

--- cmd/deploy/remote.go
+++ cmd/deploy/remote.go
@@ -14,29 +14,74 @@
 package deploy
 
 import (
-	"bytes"
 	"context"
-	"fmt"
+	"math/rand"
 	"os"
+	"text/template"
 
 	buildV1 "github.com/okteto/okteto/cmd/build/v1"
 	buildv2 "github.com/okteto/okteto/cmd/build/v2"
 	"github.com/okteto/okteto/pkg/cmd/build"
 	"github.com/okteto/okteto/pkg/constants"
+	oktetoLog "github.com/okteto/okteto/pkg/log"
 	"github.com/okteto/okteto/pkg/model"
 	"github.com/okteto/okteto/pkg/okteto"
 	"github.com/okteto/okteto/pkg/types"
 	"github.com/spf13/afero"
 )
 
+const (
+	oktetoCLIImage     = "gcr.io/development-300207/okteto:remote-deploy"
+	dockerfileTemplate = `
+FROM {{ .OktetoCLIImage }} as okteto-cli
+
+FROM alpine as certs
+RUN apk update && apk add ca-certificates
+
+FROM {{ .UserDeployImage }} as deploy
+
+RUN mkdir -p /usr/local/bin/ && cp /app/bin/* /usr/local/bin/
+COPY --from=certs /etc/ssl/certs /etc/ssl/certs
+COPY --from=okteto-cli /usr/local/bin/okteto /usr/local/bin/okteto
+
+{{range $key, $val := .OktetoBuildEnvVars }}
+ENV {{$key}} {{$val}}
+{{end}}
+ENV {{ .NamespaceEnvVar }} {{ .NamespaceValue }}
+ENV {{ .ContextEnvVar }} {{ .ContextValue }}
+ENV {{ .TokenEnvVar }} {{ .TokenValue }}
+ENV {{ .RemoteDeployEnvVar }} true
+
+COPY . /okteto/app
+WORKDIR /okteto/app
+
+ENV OKTETO_INVALIDATE_CACHE {{ .RandomInt }}
+RUN okteto deploy --log-output=json
+`
+)
+
+type dockerfileTemplateProperties struct {
+	OktetoCLIImage     string
+	UserDeployImage    string
+	OktetoBuildEnvVars map[string]string
+	ContextEnvVar      string
+	ContextValue       string
+	NamespaceEnvVar    string
+	NamespaceValue     string
+	TokenEnvVar        string
+	TokenValue         string
+	RemoteDeployEnvVar string
+	RandomInt          int
+}
+
 type remoteDeployCommand struct {
-	image     string
-	namespace string
-	deployer  localDeployer
+	builder *buildv2.OktetoBuilder
 }
 
 func newRemoteDeployer() *remoteDeployCommand {
-	return &remoteDeployCommand{}
+	return &remoteDeployCommand{
+		builder: buildv2.NewBuilderFromScratch(),
+	}
 }
 
 func (rd *remoteDeployCommand) deploy(ctx context.Context, deployOptions *Options) error {
@@ -53,23 +98,27 @@ func (rd *remoteDeployCommand) deploy(ctx context.Context, deployOptions *Option
 
 	setDeployOptionsValuesFromManifest(ctx, deployOptions, cwd, c)
 
-	if err := buildImages(ctx, buildv2.NewBuilderFromScratch().Build, buildv2.NewBuilderFromScratch().GetServicesToBuild, deployOptions); err != nil {
+	if err := buildImages(ctx, rd.builder.Build, rd.builder.GetServicesToBuild, deployOptions); err != nil {
 		return err
 	}
 
-	imageStepsUsedToDeploy := [][]byte{
-		[]byte("FROM fokingwone/okteto as okteto-cli"),
-		[]byte("FROM bitnami/kubectl as kubectl"),
-		[]byte(fmt.Sprintf("FROM %s as deploy", deployOptions.Manifest.Deploy.Image)),
-		[]byte(fmt.Sprintf("ENV %s %s", model.OktetoContextEnvVar, okteto.Context().Name)),
-		[]byte(fmt.Sprintf("ENV %s %s", model.OktetoTokenEnvVar, okteto.Context().Token)),
-		[]byte(fmt.Sprintf("ENV %s true", constants.OKtetoDeployRemote)),
-		[]byte("COPY --from=okteto-cli /usr/local/bin/okteto /usr/local/bin/okteto"),
-		[]byte("COPY --from=kubectl /opt/bitnami/kubectl/bin/kubectl /usr/local/bin/kubectl"),
-		[]byte("COPY . ."),
-		[]byte("RUN okteto deploy"),
+	tmpl, err := template.New("dockerfile").Parse(dockerfileTemplate)
+	if err != nil {
+		return err
 	}
-	dockerfileContent := bytes.Join(imageStepsUsedToDeploy, []byte("\n"))
+	dockerfileSyntax := dockerfileTemplateProperties{
+		OktetoCLIImage:     oktetoCLIImage,
+		UserDeployImage:    deployOptions.Manifest.Deploy.Image,
+		OktetoBuildEnvVars: rd.builder.GetBuildEnvVars(),
+		ContextEnvVar:      model.OktetoContextEnvVar,
+		ContextValue:       okteto.Context().Name,
+		NamespaceEnvVar:    model.OktetoNamespaceEnvVar,
+		NamespaceValue:     okteto.Context().Namespace,
+		TokenEnvVar:        model.OktetoTokenEnvVar,
+		TokenValue:         okteto.Context().Token,
+		RemoteDeployEnvVar: constants.OKtetoDeployRemote,
+		RandomInt:          rand.Intn(1000),
+	}
 
 	fs := afero.NewOsFs()
 
@@ -77,8 +126,13 @@ func (rd *remoteDeployCommand) deploy(ctx context.Context, deployOptions *Option
 	if err != nil {
 		return err
 	}
-	err = afero.WriteFile(fs, dockerfile.Name(), dockerfileContent, 0600)
-	if err != nil {
+
+	defer func() {
+		if err := fs.Remove(dockerfile.Name()); err != nil {
+			oktetoLog.Infof("error removing dockerfile: %w", err)
+		}
+	}()
+	if err := tmpl.Execute(dockerfile, dockerfileSyntax); err != nil {
 		return err
 	}
 
@@ -86,7 +140,7 @@ func (rd *remoteDeployCommand) deploy(ctx context.Context, deployOptions *Option
 		Dockerfile: dockerfile.Name(),
 	}
 
-	buildOptions := build.OptsFromBuildInfo("", "", buildInfo, &types.BuildOptions{Path: cwd})
+	buildOptions := build.OptsFromBuildInfo("", "", buildInfo, &types.BuildOptions{Path: cwd, OutputMode: "deploy"})
 	buildOptions.Tag = ""
 	if err := buildV1.NewBuilderFromScratch().Build(ctx, buildOptions); err != nil {
 		return err
