b0fe07821b1d5763f31cb69f02139eb28a876cbd
adripedriza <adripedriza@gmail.com>
2023-01-17 17:19:24 +0100

    refactor test
    Signed-off-by: adripedriza <adripedriza@gmail.com>

--- pkg/model/serializer_test.go
+++ pkg/model/serializer_test.go
@@ -22,6 +22,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/okteto/okteto/pkg/constants"
 	"github.com/okteto/okteto/pkg/externalresource"
 	"github.com/okteto/okteto/pkg/model/forward"
 	"github.com/stretchr/testify/assert"
@@ -1837,7 +1838,7 @@ func TestDeployInfoUnmarshalling(t *testing.T) {
 						Command: "okteto stack deploy",
 					},
 				},
-				Image: "gcr.io/development-300207/okteto:remote-deploy",
+				Image: constants.OktetoCLIImageForRemote,
 			},
 		},
 		{
