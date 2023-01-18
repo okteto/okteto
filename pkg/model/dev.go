949ff59d42e8702bb6a283dd99aa3088b9cf0002
Dave Chesnut <7511912+skikd636@users.noreply.github.com>
2023-01-18 10:06:36 -0500

    Add parameter for pass thru of allowPrivilegeEscalation (#3291) (#3292)
    * Add parameter for pass thru of allowPrivilegeEscalation (#3291)
    
    Signed-off-by: Dave Chesnut <7511912+skikd636@users.noreply.github.com>
    
    * Added unit tests for #3291
    
    Signed-off-by: Dave Chesnut <7511912+skikd636@users.noreply.github.com>
    Co-authored-by: Chesnut, Dave (IMS) <chesnutd@imsweb.com>

--- pkg/model/dev.go
+++ pkg/model/dev.go
@@ -276,11 +276,12 @@ type Duration time.Duration
 
 // SecurityContext represents a pod security context
 type SecurityContext struct {
-	RunAsUser    *int64        `json:"runAsUser,omitempty" yaml:"runAsUser,omitempty"`
-	RunAsGroup   *int64        `json:"runAsGroup,omitempty" yaml:"runAsGroup,omitempty"`
-	FSGroup      *int64        `json:"fsGroup,omitempty" yaml:"fsGroup,omitempty"`
-	Capabilities *Capabilities `json:"capabilities,omitempty" yaml:"capabilities,omitempty"`
-	RunAsNonRoot *bool         `json:"runAsNonRoot,omitempty" yaml:"runAsNonRoot,omitempty"`
+	RunAsUser    *int64              `json:"runAsUser,omitempty" yaml:"runAsUser,omitempty"`
+	RunAsGroup   *int64              `json:"runAsGroup,omitempty" yaml:"runAsGroup,omitempty"`
+	FSGroup      *int64              `json:"fsGroup,omitempty" yaml:"fsGroup,omitempty"`
+	Capabilities *Capabilities       `json:"capabilities,omitempty" yaml:"capabilities,omitempty"`
+	RunAsNonRoot *bool               `json:"runAsNonRoot,omitempty" yaml:"runAsNonRoot,omitempty"`
+        AllowPrivilegeEscalation *bool   `json:"allowPrivilegeEscalation,omitempty" yaml:"allowPrivilegeEscalation,omitempty"`
 }
 
 // Capabilities sets the linux capabilities of a container
