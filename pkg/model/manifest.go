4dc77e8c680869f88aceb8215c6fe36f6c0f0497
Pablo Chico de Guzman <pchico83@gmail.com>
2023-03-07 02:07:10 +0100

    Expand envvars in divert.namespace field
    Signed-off-by: Pablo Chico de Guzman <pchico83@gmail.com>

--- pkg/model/manifest.go
+++ pkg/model/manifest.go
@@ -788,7 +788,6 @@ func (m *Manifest) validateDivert() error {
 
 	switch m.Deploy.Divert.Driver {
 	case OktetoDivertWeaverDriver:
-		break
 	case OktetoDivertIstioDriver:
 		if m.Deploy.Divert.Service == "" {
 			return fmt.Errorf("the field 'deploy.divert.service' is mandatory")
@@ -812,11 +811,15 @@ func (m *Manifest) validateDivert() error {
 
 func (m *Manifest) setDefaults() error {
 	if m.Deploy != nil && m.Deploy.Divert != nil {
+		var err error
 		if m.Deploy.Divert.Driver == "" {
 			m.Deploy.Divert.Driver = OktetoDivertWeaverDriver
 		}
+		m.Deploy.Divert.Namespace, err = ExpandEnv(m.Deploy.Divert.Namespace, false)
+		if err != nil {
+			return err
+		}
 		for i := range m.Deploy.Divert.Hosts {
-			var err error
 			m.Deploy.Divert.Hosts[i].VirtualService, err = ExpandEnv(m.Deploy.Divert.Hosts[i].VirtualService, false)
 			if err != nil {
 				return err
