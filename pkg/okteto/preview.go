71825c72fa1d37d372e357c280f4586ec90812b0
Javier López Barba <javier@okteto.com>
2023-03-23 11:44:25 +0100

    remove unused code
    Signed-off-by: Javier López Barba <javier@okteto.com>

--- pkg/okteto/preview.go
+++ pkg/okteto/preview.go
@@ -15,8 +15,6 @@ package okteto
 
 import (
 	"context"
-	"fmt"
-	"regexp"
 	"strings"
 
 	"github.com/okteto/okteto/pkg/config"
@@ -308,26 +306,4 @@ func (*previewClient) translateErr(err error, name string) error {
 			Hint: "Please log in with an administrator account or use a personal preview environment"}
 	}
 	return err
-}
-
-// validateNamespace checks if the new ns exceeds the max chars allowed by k8s and if t
-func validateNamespace(namespace, object string) error {
-	if len(namespace) > MAX_ALLOWED_CHARS {
-		return oktetoErrors.UserError{
-			E: namespaceValidationError{
-				object: object,
-			},
-			Hint: fmt.Sprintf("%s name must be shorter than 63 characters.", object),
-		}
-	}
-	nameValidationRegex := regexp.MustCompile("^[a-z0-9]([-a-z0-9]*[a-z0-9])?$")
-	if !nameValidationRegex.MatchString(namespace) {
-		return oktetoErrors.UserError{
-			E: namespaceValidationError{
-				object: object,
-			},
-			Hint: fmt.Sprintf("%s name must consist of lower case alphanumeric characters or '-', and must start and end with an alphanumeric character", object),
-		}
-	}
-	return nil
 }
