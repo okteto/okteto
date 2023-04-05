cab6d58ab27c5e2b97e8a8ec9d221e2071c2d19c
Javier López Barba <javier@okteto.com>
2023-04-05 17:51:23 +0200

    fix: adding .git folder to the folder should propagate okteto git commit (#3533)
    * fix: adding .git folder to the folder should propagate okteto git commit
    
    Signed-off-by: Javier López Barba <javier@okteto.com>
    
    * fix: add git commit to destroy
    
    Signed-off-by: Javier López Barba <javier@okteto.com>
    
    * refactor: move constants to constants package
    
    Signed-off-by: Javier López Barba <javier@okteto.com>
    
    * fix: unit tests
    
    Signed-off-by: Javier López Barba <javier@okteto.com>
    
    * refactor: rename struct
    
    Signed-off-by: Javier López Barba <javier@okteto.com>
    
    ---------
    
    Signed-off-by: Javier López Barba <javier@okteto.com>

---
+++ pkg/repository/remote_test.go
@@ -0,0 +1,47 @@
+// Copyright 2023 The Okteto Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package repository
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRemoteIsCleanTrue(t *testing.T) {
+	remote := oktetoRemoteRepoController{
+		gitCommit: "123",
+	}
+	isClean, err := remote.isClean()
+	assert.NoError(t, err)
+	assert.True(t, isClean)
+}
+
+func TestRemoteIsCleanFalse(t *testing.T) {
+	remote := oktetoRemoteRepoController{
+		gitCommit: "",
+	}
+	isClean, err := remote.isClean()
+	assert.NoError(t, err)
+	assert.False(t, isClean)
+}
+
+func TestRemoteGetSHA(t *testing.T) {
+	remote := oktetoRemoteRepoController{
+		gitCommit: "123",
+	}
+	sha, err := remote.getSHA()
+	assert.NoError(t, err)
+	assert.Equal(t, remote.gitCommit, sha)
+}
