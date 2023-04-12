b2383784ca77482c0687dc9caf0ca840491e87ad
Agustín Díaz <agustin.ramiro.diaz@gmail.com>
2023-04-12 18:12:11 +0100

    Add test files
    Signed-off-by: Agustín Díaz <agustin.ramiro.diaz@gmail.com>

--- pkg/okteto/kubetoken/cache_test.go
+++ pkg/okteto/kubetoken/cache_test.go
@@ -4,13 +4,7 @@ import "testing"
 
 func TestFileCache(t *testing.T) {
 
-	// Test file does not exist
-
 	// Test file has corrupted data
-
-	// Test error while writing file
-
-	// Test error while stat file
 
 	// Test token expiration
 
---
+++ pkg/okteto/kubetoken/store_test.go
@@ -0,0 +1,13 @@
+package kubetoken
+
+import "testing"
+
+func TestByteStore(t *testing.T) {
+
+	// Test file does not exist
+
+	// Test error while writing file
+
+	// Test error while stat file
+
+}
