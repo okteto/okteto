d7d11866ddfac104197f093e89b9f7738e4cdf35
Nitish Kumar <justnitish06@gmail.com>
2023-04-20 14:05:27 +0530

    Added new commands for okteto namespace - sleep and wake (#3479)
    * [WIP]Added new commands for namespace - sleep and wake
    
    * changes applied
    
    Signed-off-by: NitishKumar06 <justnitish06@gmail.com>
    
    ---------
    
    Signed-off-by: NitishKumar06 <justnitish06@gmail.com>

--- cmd/namespace/errors.go
+++ cmd/namespace/errors.go
@@ -7,3 +7,7 @@ import (
 var errFailedDeleteNamespace = errors.New("failed to delete namespace")
 
 var errDeleteNamespaceTimeout = errors.New("namespace delete didn't finish")
+
+var errFailedSleepNamespace = errors.New("failed to sleep namespace")
+
+var errFailedWakeNamespace = errors.New("failed to wake namespace")
---
+++ cmd/namespace/sleep.go
@@ -0,0 +1,67 @@
+// Copyright 2023 The Okteto Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package namespace
+
+import (
+	"context"
+	"fmt"
+	contextCMD "github.com/okteto/okteto/cmd/context"
+	"github.com/okteto/okteto/cmd/utils"
+	oktetoErrors "github.com/okteto/okteto/pkg/errors"
+	oktetoLog "github.com/okteto/okteto/pkg/log"
+	"github.com/okteto/okteto/pkg/okteto"
+	"github.com/spf13/cobra"
+)
+
+// Sleep sleeps a namespace
+func Sleep(ctx context.Context) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "sleep <name>",
+		Short: "Sleeps a namespace",
+		Args:  utils.ExactArgsAccepted(1, ""),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			nsToSleep := args[0]
+			if err := contextCMD.NewContextCommand().Run(ctx, &contextCMD.ContextOptions{Namespace: nsToSleep, Show: true}); err != nil {
+				return err
+			}
+
+			if !okteto.IsOkteto() {
+				return oktetoErrors.ErrContextIsNotOktetoCluster
+			}
+
+			nsCmd, err := NewCommand()
+			if err != nil {
+				return err
+			}
+			err = nsCmd.ExecuteSleepNamespace(ctx, nsToSleep)
+			return err
+		},
+	}
+	return cmd
+}
+
+func (nc *NamespaceCommand) ExecuteSleepNamespace(ctx context.Context, namespace string) error {
+	// Spinner to be loaded before sleeping a namespace
+	oktetoLog.Spinner(fmt.Sprintf("Sleeping %s namespace", namespace))
+	oktetoLog.StartSpinner()
+	defer oktetoLog.StopSpinner()
+
+	// trigger namespace to sleep
+	if err := nc.okClient.Namespaces().Sleep(ctx, namespace); err != nil {
+		return fmt.Errorf("%w: %v", errFailedSleepNamespace, err)
+	}
+
+	oktetoLog.Success("Namespace '%s' is sleeping", namespace)
+	return nil
+}
