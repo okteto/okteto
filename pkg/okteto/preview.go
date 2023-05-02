3d783b8ee95778c98351ecea5fc2207c57e55c69
Francisco Miranda <franciscomirandaa@gmail.com>
2023-05-02 17:17:28 -0300

    Add external resources to preview endpoints

--- pkg/okteto/preview.go
+++ pkg/okteto/preview.go
@@ -84,8 +84,10 @@ type resourceInfo struct {
 	DeployedBy graphql.String
 }
 type previewEndpoints struct {
+	Endpoints    []endpointURL
 	Deployments  []deploymentEndpoint
 	Statefulsets []statefulsetEdnpoint
+	Externals    []externalEndpoints
 }
 
 type deploymentEndpoint struct {
@@ -93,6 +95,9 @@ type deploymentEndpoint struct {
 }
 
 type statefulsetEdnpoint struct {
+	Endpoints []endpointURL
+}
+type externalEndpoints struct {
 	Endpoints []endpointURL
 }
 
@@ -240,20 +245,39 @@ func (c *previewClient) ListEndpoints(ctx context.Context, previewName string) (
 		return nil, err
 	}
 
-	for _, d := range queryStruct.Response.Deployments {
-		for _, endpoint := range d.Endpoints {
-			endpoints = append(endpoints, types.Endpoint{
-				URL: string(endpoint.Url),
-			})
-		}
+	var endpointsMap = map[graphql.String]bool{}
+
+	for _, endpoint := range queryStruct.Response.Endpoints {
+		endpointsMap[endpoint.Url] = true
 	}
 
+	// @francisco - 2023//05/02
+	// The backend Endpoints field was being return empty in some older versions
+	// of the okteto api. Lets make sure we correctly resolve the endpoints from
+	// the known resources. All this below should be safe to remove in newer versions
+	// of the okteto cli
+	// <-- LEGACY START -->
+	for _, d := range queryStruct.Response.Deployments {
+		for _, endpoint := range d.Endpoints {
+			endpointsMap[endpoint.Url] = true
+		}
+	}
 	for _, sfs := range queryStruct.Response.Statefulsets {
 		for _, endpoint := range sfs.Endpoints {
-			endpoints = append(endpoints, types.Endpoint{
-				URL: string(endpoint.Url),
-			})
+			endpointsMap[endpoint.Url] = true
 		}
+	}
+	for _, ext := range queryStruct.Response.Externals {
+		for _, endpoint := range ext.Endpoints {
+			endpointsMap[endpoint.Url] = true
+		}
+	}
+	// <-- LEGACY END -->
+
+	for url := range endpointsMap {
+		endpoints = append(endpoints, types.Endpoint{
+			URL: string(url),
+		})
 	}
 	return endpoints, nil
 }
