2a295b19bb9796678240e4d0df83cfe1ebd054f3
Pablo Chico de Guzman <pchico83@gmail.com>
2023-05-25 19:17:35 +0200

    Fix RC in divert (weaver) when the cache has outdated information (#3655)
    * Fix RC in divert (weaver) when the cache has outdated information
    
    Signed-off-by: Pablo Chico de Guzman <pchico83@gmail.com>
    
    * Move from "reflect" to "assert"
    
    Signed-off-by: Pablo Chico de Guzman <pchico83@gmail.com>
    
    ---------
    
    Signed-off-by: Pablo Chico de Guzman <pchico83@gmail.com>
    Signed-off-by: adrianpedriza <adripedriza@gmail.com>

--- pkg/divert/weaver/services_test.go
+++ pkg/divert/weaver/services_test.go
@@ -14,10 +14,10 @@
 package weaver
 
 import (
-	"reflect"
 	"testing"
 
 	"github.com/okteto/okteto/pkg/model"
+	"github.com/stretchr/testify/assert"
 	apiv1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
@@ -115,9 +115,7 @@ func Test_translateService(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			result, _ := translateService("test", "cindy", tt.s)
-			if !reflect.DeepEqual(result, tt.expected) {
-				t.Fatalf("test %s failed: %v vs %s", tt.name, result, tt.expected)
-			}
+			assert.Equal(t, result, tt.expected)
 		})
 	}
 }
