ebe5d55cacdec3a8f03ef478c4861e45a04fd581
Agustín Díaz <49416765+AgustinRamiroDiaz@users.noreply.github.com>
2023-06-26 14:44:41 -0300

    Fix kubetoken usage message (#3606)
    Signed-off-by: Agustín Díaz <agustin.ramiro.diaz@gmail.com>

--- cmd/kubetoken/kubetoken.go
+++ cmd/kubetoken/kubetoken.go
@@ -27,7 +27,7 @@ import (
 
 func KubeToken() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "kubetoken <context> <namespace>",
+		Use:   "kubetoken",
 		Short: "Print Kubernetes cluster credentials in ExecCredential format.",
 		Long: `Print Kubernetes cluster credentials in ExecCredential format.
 You can find more information on 'ExecCredential' and 'client side authentication' at (https://kubernetes.io/docs/reference/config-api/client-authentication.v1/) and  https://kubernetes.io/docs/reference/access-authn-authz/authentication/#client-go-credential-plugins`,
