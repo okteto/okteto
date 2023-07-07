f738ae00659947fafbf0e75a0ab94828b574f411
Rakshit Gondwal <98955085+rakshitgondwal@users.noreply.github.com>
2023-07-07 18:00:43 +0530

    feat: add okteto preview `sleep|wake` command (#3570)
    * feat: add okteto preview sleep|wake commands
    
    Signed-off-by: Rakshit Gondwal <rakshitgondwal3@gmail.com>
    
    * update preview sleep|wake endpoints
    
    Signed-off-by: Rakshit Gondwal <rakshitgondwal3@gmail.com>
    
    * fix: after review
    
    Signed-off-by: Rakshit Gondwal <rakshitgondwal3@gmail.com>
    
    * fix after review
    
    Signed-off-by: Rakshit Gondwal <rakshitgondwal3@gmail.com>
    
    * test: add tests for preview sleep|wake
    
    Signed-off-by: Rakshit Gondwal <rakshitgondwal3@gmail.com>
    
    * remove unused functions
    
    Signed-off-by: Rakshit Gondwal <rakshitgondwal3@gmail.com>
    
    * test: move fake to test file and remove duplicated test
    
    Signed-off-by: Teresa Romero <teresa@okteto.com>
    
    ---------
    
    Signed-off-by: Rakshit Gondwal <rakshitgondwal3@gmail.com>
    Signed-off-by: Teresa Romero <teresa@okteto.com>
    Co-authored-by: Teresa Romero <teresa@okteto.com>

---
+++ cmd/namespace/namespace_test.go
@@ -0,0 +1,43 @@
+package namespace
+
+import (
+	contextCMD "github.com/okteto/okteto/cmd/context"
+	"github.com/okteto/okteto/internal/test"
+	"github.com/okteto/okteto/internal/test/client"
+	"github.com/okteto/okteto/pkg/k8s/ingresses"
+	"github.com/okteto/okteto/pkg/types"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	clientcmdapi "k8s.io/client-go/tools/clientcmd/api"
+)
+
+type fakeK8sProvider struct {
+	k8sClient kubernetes.Interface
+}
+
+func (p *fakeK8sProvider) Provide(_ *clientcmdapi.Config) (kubernetes.Interface, *rest.Config, error) {
+	return p.k8sClient, nil, nil
+}
+
+func (*fakeK8sProvider) GetIngressClient() (*ingresses.Client, error) {
+	return nil, nil
+}
+
+func newFakeContextCommand(c *client.FakeOktetoClient, user *types.User) *contextCMD.ContextCommand {
+	return &contextCMD.ContextCommand{
+		OktetoClientProvider: client.NewFakeOktetoClientProvider(c),
+		K8sClientProvider:    test.NewFakeK8sProvider(nil),
+		LoginController:      test.NewFakeLoginController(user, nil),
+		OktetoContextWriter:  test.NewFakeOktetoContextWriter(),
+	}
+}
+
+func NewFakeNamespaceCommand(okClient *client.FakeOktetoClient, k8sClient kubernetes.Interface, user *types.User) *NamespaceCommand {
+	return &NamespaceCommand{
+		okClient: okClient,
+		ctxCmd:   newFakeContextCommand(okClient, user),
+		k8sClientProvider: &fakeK8sProvider{
+			k8sClient: k8sClient,
+		},
+	}
+}
