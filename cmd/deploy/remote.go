d9e4fddfb2d475655e7dcf1005e6a388c550ee22
adripedriza <adripedriza@gmail.com>
2023-08-04 11:26:59 +0200

    fix: compute build envs in local (#3874)
    * compute build envs in local
    
    Signed-off-by: adrianpedriza <adripedriza@gmail.com>
    
    * inject build env vars getter
    
    Signed-off-by: adrianpedriza <adripedriza@gmail.com>
    
    * fix test for non propagated --log-level flag
    
    Signed-off-by: adrianpedriza <adripedriza@gmail.com>
    
    * optimize dockerfile in terms of cache
    
    Signed-off-by: adrianpedriza <adripedriza@gmail.com>
    
    ---------
    
    Signed-off-by: adrianpedriza <adripedriza@gmail.com>

--- cmd/deploy/remote.go
+++ cmd/deploy/remote.go
@@ -55,6 +55,7 @@ FROM {{ .UserDeployImage }} as deploy
 ENV PATH="${PATH}:/okteto/bin"
 COPY --from=okteto-cli /usr/local/bin/* /okteto/bin/
 
+
 ENV {{ .RemoteDeployEnvVar }} true
 ARG {{ .NamespaceArgName }}
 ARG {{ .ContextArgName }}
@@ -67,6 +68,10 @@ RUN echo "${{ .TlsCertBase64ArgName }}" | base64 -d > /etc/ssl/certs/okteto.crt
 
 COPY . /okteto/src
 WORKDIR /okteto/src
+
+{{range $key, $val := .OktetoBuildEnvVars }}
+ENV {{$key}} {{$val}}
+{{end}}
 
 ARG {{ .GitCommitArgName }}
 ARG {{ .InvalidateCacheArgName }}
@@ -85,6 +90,7 @@ type dockerfileTemplateProperties struct {
 	OktetoCLIImage         string
 	UserDeployImage        string
 	RemoteDeployEnvVar     string
+	OktetoBuildEnvVars     map[string]string
 	ContextArgName         string
 	NamespaceArgName       string
 	TokenArgName           string
@@ -97,7 +103,7 @@ type dockerfileTemplateProperties struct {
 }
 
 type remoteDeployCommand struct {
-	builderV2            *buildv2.OktetoBuilder
+	getBuildEnvVars      func() map[string]string
 	builderV1            builder.Builder
 	fs                   afero.Fs
 	workingDirectoryCtrl filesystem.WorkingDirectoryInterface
@@ -115,7 +121,7 @@ type remoteDeployCommand struct {
 func newRemoteDeployer(builder *buildv2.OktetoBuilder) *remoteDeployCommand {
 	fs := afero.NewOsFs()
 	return &remoteDeployCommand{
-		builderV2:            builder,
+		getBuildEnvVars:      builder.GetBuildEnvVars,
 		builderV1:            remoteBuild.NewBuilderFromScratch(),
 		fs:                   fs,
 		workingDirectoryCtrl: filesystem.NewOsWorkingDirectoryCtrl(),
@@ -262,6 +268,7 @@ func (rd *remoteDeployCommand) createDockerfile(tmpDir string, opts *Options) (s
 		UserDeployImage:        opts.Manifest.Deploy.Image,
 		RemoteDeployEnvVar:     constants.OktetoDeployRemote,
 		ContextArgName:         model.OktetoContextEnvVar,
+		OktetoBuildEnvVars:     rd.getBuildEnvVars(),
 		NamespaceArgName:       model.OktetoNamespaceEnvVar,
 		TlsCertBase64ArgName:   constants.OktetoTlsCertBase64EnvVar,
 		InternalServerName:     constants.OktetoInternalServerNameEnvVar,
