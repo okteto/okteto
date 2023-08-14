b2ca2b4d2673e0ca4bf37f69947925ab2773cc6e
Andrea Falzetti <andrea@falzetti.me>
2023-08-14 17:55:35 +0100

    fix: check OktetoUseStaticKubetokenEnvVar
    Signed-off-by: Andrea Falzetti <andrea@falzetti.me>

--- cmd/context/create.go
+++ cmd/context/create.go
@@ -381,17 +381,14 @@ func (c ContextCommand) getUserContext(ctx context.Context, ns string) (*types.U
 			}
 		}
 
-		kubetoken, err := client.Kubetoken().GetKubeToken(okteto.Context().Name, okteto.Context().Namespace)
-		if err != nil {
-			if utils.LoadBoolean(model.OktetoUseStaticKubetokenEnvVar) {
-				oktetoLog.Info("You opted-in to use static kubernetes tokens because you set '%s', however the feature is not available and okteto CLI will be using a static token", model.OktetoUseStaticKubetokenEnvVar)
-			}
-			oktetoLog.Debug("Dynamic kubernetes tokens not available: falling back to static token")
+		if utils.LoadBoolean(model.OktetoUseStaticKubetokenEnvVar) {
+			oktetoLog.Debug("You opted-in to use static kubernetes tokens because you set '%s'. This feature will be removed in the future. We recommend using dynamic kubernetes tokens, to know more check out out documentation: https://www.okteto.com/docs", model.OktetoUseStaticKubetokenEnvVar)
 			return userContext, nil
 		}
 
-		if utils.LoadBoolean(model.OktetoUseStaticKubetokenEnvVar) {
-			oktetoLog.Info("You are using static kubernetes tokens because you set '%s'. This feature will be removed in the future. We recommend using dynamic kubernetes tokens, to know more check out out documentation: https://www.okteto.com/docs", model.OktetoUseStaticKubetokenEnvVar)
+		kubetoken, err := client.Kubetoken().GetKubeToken(okteto.Context().Name, okteto.Context().Namespace)
+		if err != nil {
+			oktetoLog.Debug("Dynamic kubernetes tokens not available: falling back to static token")
 			return userContext, nil
 		}
 
