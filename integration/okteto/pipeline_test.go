e31ffc10e024ee81a53e26432825e60cdde5c582
Tere <teresa@okteto.com>
2023-09-28 12:43:16 +0000

    New option `--reuse-params` at `okteto pipeline deploy` (#3985)
    * refactor: added new flag reuse-params to okteto pipeline deploy
    
    Signed-off-by: Teresa Romero <teresa@okteto.com>
    
    * refactor: implement reuse params at ExecuteDeployPipeline
    
    Signed-off-by: Teresa Romero <teresa@okteto.com>
    
    * refactor: unify configmaps.Get for skipifexists and reuseparams options
    
    Signed-off-by: Teresa Romero <teresa@okteto.com>
    
    * refactor: remove configmap getter from setEnvsFromDependency
    
    Signed-off-by: Teresa Romero <teresa@okteto.com>
    
    * refactor: cfgToDeployOptions to param configmap
    
    Signed-off-by: Teresa Romero <teresa@okteto.com>
    
    * test: add integration for labels and reuse flag
    
    Signed-off-by: Teresa Romero <teresa@okteto.com>
    
    * refactor: create error and update copy for dev environment not found
    
    Signed-off-by: Teresa Romero <teresa@okteto.com>
    
    * test: add cases at pipeline unit test with reuse params flags
    
    Signed-off-by: Teresa Romero <teresa@okteto.com>
    
    * test: add test for ExecuteDeployPipeline
    
    Signed-off-by: Teresa Romero <teresa@okteto.com>
    
    * test: add test case for setEnvsFromDependency
    
    Signed-off-by: Teresa Romero <teresa@okteto.com>
    
    ---------
    
    Signed-off-by: Teresa Romero <teresa@okteto.com>

--- integration/okteto/pipeline_test.go
+++ integration/okteto/pipeline_test.go
@@ -17,11 +17,15 @@
 package okteto
 
 import (
+	"context"
 	"fmt"
+	"path/filepath"
 	"testing"
 
 	"github.com/okteto/okteto/integration"
 	"github.com/okteto/okteto/integration/commands"
+	"github.com/okteto/okteto/pkg/k8s/kubeconfig"
+	"github.com/okteto/okteto/pkg/okteto"
 	"github.com/stretchr/testify/require"
 )
 
@@ -67,4 +71,62 @@ func TestPipelineCommand(t *testing.T) {
 		Token:      token,
 	}
 	require.NoError(t, commands.RunOktetoPipelineDestroy(oktetoPath, pipelineDestroyOptions))
+}
+
+func TestPipelineDeployWithReuse(t *testing.T) {
+	integration.SkipIfNotOktetoCluster(t)
+	t.Parallel()
+	oktetoPath, err := integration.GetOktetoPath()
+	require.NoError(t, err)
+
+	dir := t.TempDir()
+	testNamespace := integration.GetTestNamespace("TestPipelineDeployWithReuse", user)
+	namespaceOpts := &commands.NamespaceOptions{
+		Namespace:  testNamespace,
+		OktetoHome: dir,
+		Token:      token,
+	}
+	require.NoError(t, commands.RunOktetoCreateNamespace(oktetoPath, namespaceOpts))
+	defer commands.RunOktetoDeleteNamespace(oktetoPath, namespaceOpts)
+
+	require.NoError(t, commands.RunOktetoKubeconfig(oktetoPath, dir))
+	c, _, err := okteto.NewK8sClientProvider().Provide(kubeconfig.Get([]string{filepath.Join(dir, ".kube", "config")}))
+	require.NoError(t, err)
+
+	pipelineOptions := &commands.DeployPipelineOptions{
+		Namespace:  testNamespace,
+		Repository: "https://github.com/okteto/go-getting-started",
+		Branch:     "cli-e2e",
+		Wait:       true,
+		OktetoHome: dir,
+		Token:      token,
+		Name:       "test-pipeline",
+		Labels:     []string{"test-label"},
+	}
+	require.NoError(t, commands.RunOktetoDeployPipeline(oktetoPath, pipelineOptions))
+
+	cmap, err := integration.GetConfigmap(context.Background(), testNamespace, "okteto-git-test-pipeline", c)
+	require.NoError(t, err)
+
+	require.Contains(t, cmap.Labels, "label.okteto.com/test-label")
+
+	pipelineOptionsWithReuse := &commands.DeployPipelineOptions{
+		Namespace:   testNamespace,
+		Repository:  "https://github.com/okteto/go-getting-started",
+		Branch:      "ignored-value",
+		Wait:        true,
+		OktetoHome:  dir,
+		Token:       token,
+		Name:        "test-pipeline",
+		ReuseParams: true,
+	}
+
+	require.NoError(t, commands.RunOktetoDeployPipeline(oktetoPath, pipelineOptionsWithReuse))
+
+	cmapRedeploy, err := integration.GetConfigmap(context.Background(), testNamespace, "okteto-git-test-pipeline", c)
+	require.NoError(t, err)
+
+	require.Contains(t, cmapRedeploy.Labels, "label.okteto.com/test-label")
+	require.Equal(t, cmap.CreationTimestamp, cmapRedeploy.CreationTimestamp)
+
 }
