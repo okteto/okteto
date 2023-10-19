a8fe89e98f02a8ee4a81ddd7147bf35c9533eaa0
Tere <teresa@okteto.com>
2023-10-19 05:45:08 +0000

    [CLI Quality][Lint] add tparallel lint and fix errors (#4020)
    * chore: add tparallel lint and fix errors
    
    Signed-off-by: Teresa Romero <teresa@okteto.com>
    
    * chore: remove new-from-rev from golangci-lint config
    
    Signed-off-by: Teresa Romero <teresa@okteto.com>
    
    ---------
    
    Signed-off-by: Teresa Romero <teresa@okteto.com>

--- cmd/up/forwards_test.go
+++ cmd/up/forwards_test.go
@@ -14,6 +14,7 @@ import (
 )
 
 func TestGlobalForwarderStartsWhenRequired(t *testing.T) {
+	t.Parallel()
 	var tests = []struct {
 		name             string
 		globalFwdSection []forward.GlobalForward
