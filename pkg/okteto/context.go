585296ad847db821e03e9df0d23ef2d313e948dd
AdrianPedriza <adripedriza@gmail.com>
2023-12-14 15:59:31 +0100

    add test
    Signed-off-by: AdrianPedriza <adripedriza@gmail.com>

--- pkg/okteto/context.go
+++ pkg/okteto/context.go
@@ -472,15 +472,6 @@ func IsOktetoCloud() bool {
 	}
 }
 
-func IsOktetoCloudStateless(name string) bool {
-	switch name {
-	case CloudURL, StagingURL:
-		return true
-	default:
-		return false
-	}
-}
-
 func RemoveSchema(uri string) string {
 	u, err := url.Parse(uri)
 	if err != nil {
