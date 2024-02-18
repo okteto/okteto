13fd6f691f9af53dc590515d60c417d3ad0909f6
Javier Provecho Fernández (Okteto) <92427625+jpf-okteto@users.noreply.github.com>
2024-02-18 19:54:30 +0100

    chore: update buildkit fork to v0.12.5 (PLAT-351)
    Signed-off-by: Javier Provecho Fernández (Okteto) <jpf@okteto.com>

--- main.go
+++ main.go
@@ -49,6 +49,7 @@ import (
 	"github.com/spf13/pflag"
 	generateFigSpec "github.com/withfig/autocomplete-tools/packages/cobra"
 	utilRuntime "k8s.io/apimachinery/pkg/util/runtime"
+
 	// Load the different library for authentication
 	_ "k8s.io/client-go/plugin/pkg/client/auth/azure"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
