3fe953feb78a82f24b210d8f622bece4558668ae
Pablo Chico de Guzman <pchico83@gmail.com>
2024-03-26 13:55:46 +0100

    ASP.NET sample

---
+++ samples/aspnetcore/Controllers/HelloWorldController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace helloworld.Controllers
+{
+    [ApiController]
+    [Route("")]
+    [Route("[controller]")]
+    public class HelloWorldController : ControllerBase
+    {
+        private readonly ILogger<HelloWorldController> _logger;
+
+        public HelloWorldController(ILogger<HelloWorldController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            var message = "Hello world!";
+            return message;
+        }
+    }
+}
