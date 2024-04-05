e73a5bf60bbf82677565f8c921e18fafafcf128d
Francisco Miranda <franciscomirandaa@gmail.com>
2024-04-05 09:45:15 -0300

    linting

--- pkg/dag/doc.go
+++ pkg/dag/doc.go
@@ -10,7 +10,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-//
+
 // package dag provides generic interface for working with dependencies resolution
 // via Directed Acyclic Graphs (DAGs)
 //
