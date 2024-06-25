f93a27be5589de6153d637ac2799057c6de521c9
Teresa Romero <teresa@okteto.com>
2024-06-25 12:39:35 +0200

    refactor: add test to manifestFields at isManifestFieldNotFound
    Signed-off-by: Teresa Romero <teresa@okteto.com>

--- pkg/model/serializer.go
+++ pkg/model/serializer.go
@@ -1002,7 +1002,7 @@ func (d *ManifestDevs) UnmarshalYAML(unmarshal func(interface{}) error) error {
 }
 
 func isManifestFieldNotFound(err error) bool {
-	manifestFields := []string{"devs", "dev", "name", "icon", "variables", "deploy", "destroy", "build", "namespace", "context", "dependencies"}
+	manifestFields := []string{"devs", "dev", "name", "icon", "variables", "deploy", "destroy", "build", "namespace", "context", "dependencies", "test"}
 	for _, field := range manifestFields {
 		if strings.Contains(err.Error(), fmt.Sprintf("field %s not found", field)) {
 			return true
