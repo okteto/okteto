55c90a99b1fd35d5e2ea73643e2f7602f5531804
Javier López Barba <javier@okteto.com>
2024-07-16 16:52:10 +0200

    add metrics in order to know the usage (#4392)
    Signed-off-by: Javier Lopez <javier@okteto.com>

--- cmd/build/command.go
+++ cmd/build/command.go
@@ -128,6 +128,7 @@ func Build(ctx context.Context, ioCtrl *io.Controller, at, insights buildTracker
 				}
 			}
 
+			analytics.TrackBuildWithManifestVsDockerfile(builder.IsV1())
 			return builder.Build(ctx, options)
 		},
 	}
