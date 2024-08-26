c817ac2d7aa72bf0223ed816e891a6eef78efdf6
Javier Lopez <javier@okteto.com>
2024-08-26 12:56:29 +0200

    chore: update OktetoBinImageTag and syncthingVersion
    Update OktetoBinImageTag from "jlopezbarb/bin-image:1.6.0" to "jlopezbarb/bin:1.6.0" and syncthingVersion from "1.27.6" to "1.27.10"
    
    Signed-off-by: Javier Lopez <javier@okteto.com>

--- pkg/model/dev.go
+++ pkg/model/dev.go
@@ -43,7 +43,7 @@ import (
 
 var (
 	// OktetoBinImageTag image tag with okteto internal binaries
-	OktetoBinImageTag = "jlopezbarb/bin-image:1.6.0"
+	OktetoBinImageTag = "jlopezbarb/bin:1.6.0"
 
 	errBadName = fmt.Errorf("Invalid name: must consist of lower case alphanumeric characters or '-', and must start and end with an alphanumeric character")
 
