a9fd0b4198e6c343a4cdaf5716423deeeb17d1eb
Javier Lopez <javier@okteto.com>
2024-09-12 15:47:55 +0200

    debug no
    Signed-off-by: Javier Lopez <javier@okteto.com>

--- integration/deploy/deploy_remote_test.go
+++ integration/deploy/deploy_remote_test.go
@@ -63,7 +63,6 @@ func TestDeployInDeployRemote(t *testing.T) {
 		Namespace:  testNamespace,
 		OktetoHome: dir,
 		Token:      token,
-		LogLevel:   "debug",
 	}
 	require.NoError(t, commands.RunOktetoDeploy(oktetoPath, deployOptions))
 
