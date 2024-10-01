ea4b93222e11ed6990328be1f58716d0a5d6202e
Teresa Romero <teresa@okteto.com>
2024-10-01 08:56:04 +0200

    chore: add headers to files
    Signed-off-by: Teresa Romero <teresa@okteto.com>

--- pkg/validator/arguments_test.go
+++ pkg/validator/arguments_test.go
@@ -1,3 +1,16 @@
+// Copyright 2024 The Okteto Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package validator
 
 import (
@@ -17,9 +30,9 @@ func TestFileArgumentIsNotDir(t *testing.T) {
 		createFile bool
 	}
 	tests := []struct {
-		name    string
-		args    args
 		wantErr error
+		args    args
+		name    string
 	}{
 		{
 			name: "empty file",
