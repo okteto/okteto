9f4f34c9e0f5db917ce67783f4540d9a4966bf3c
Andrea Falzetti <andrea@okteto.com>
2024-11-20 16:06:28 +0100

    fix lint
    Signed-off-by: Andrea Falzetti <andrea@okteto.com>

--- cmd/generate-schema.go
+++ cmd/generate-schema.go
@@ -14,50 +14,50 @@
 package cmd
 
 import (
-    "fmt"
-    "os"
+	"fmt"
+	"os"
 
-    oktetoLog "github.com/okteto/okteto/pkg/log"
-    "github.com/okteto/okteto/pkg/schema"
-    "github.com/spf13/cobra"
+	oktetoLog "github.com/okteto/okteto/pkg/log"
+	"github.com/okteto/okteto/pkg/schema"
+	"github.com/spf13/cobra"
 )
 
 var outputFilePath string
 
 func toFile(schemaBytes []byte, outputFilePath string) error {
-    err := os.WriteFile(outputFilePath, schemaBytes, 0644)
-    if err != nil {
-        return err
-    }
-    oktetoLog.Success("Okteto JSON Schema generated and saved to: %s", outputFilePath)
+	err := os.WriteFile(outputFilePath, schemaBytes, 0644)
+	if err != nil {
+		return err
+	}
+	oktetoLog.Success("Okteto JSON Schema generated and saved to: %s", outputFilePath)
 
-    return nil
+	return nil
 }
 
 // GenerateSchema create the json schema for the okteto manifest
 func GenerateSchema() *cobra.Command {
-    cmd := &cobra.Command{
-        Args:   cobra.NoArgs,
-        Hidden: true,
-        Use:    "generate-schema",
-        Short:  "Generates the JSON Schema for the Okteto Manifest",
-        RunE: func(cmd *cobra.Command, args []string) error {
-            s := schema.NewJsonSchema()
+	cmd := &cobra.Command{
+		Args:   cobra.NoArgs,
+		Hidden: true,
+		Use:    "generate-schema",
+		Short:  "Generates the JSON Schema for the Okteto Manifest",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			s := schema.NewJsonSchema()
 
-            json, err := s.ToJSON()
-            if err != nil {
-                return err
-            }
-            if outputFilePath == "-" {
-                fmt.Print(string(json))
-                return nil
-            }
-            err = toFile(json, outputFilePath)
+			json, err := s.ToJSON()
+			if err != nil {
+				return err
+			}
+			if outputFilePath == "-" {
+				fmt.Print(string(json))
+				return nil
+			}
+			err = toFile(json, outputFilePath)
 
-            return err
-        },
-    }
+			return err
+		},
+	}
 
-    cmd.Flags().StringVarP(&outputFilePath, "output-file", "o", "-", "Path to the file where the json schema will be stored")
-    return cmd
+	cmd.Flags().StringVarP(&outputFilePath, "output-file", "o", "-", "Path to the file where the json schema will be stored")
+	return cmd
 }
