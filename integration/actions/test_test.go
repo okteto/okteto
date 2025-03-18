34433f7d3c9084fa399720bf5350eab63c31d757
Javier López Barba <javier@okteto.com>
2025-03-18 14:39:21 +0100

    add action e2e to okteto test (#4682)
    * add action e2e to okteto test
    
    Signed-off-by: Javier Lopez <javier@okteto.com>
    
    * address comments
    
    Signed-off-by: Javier Lopez <javier@okteto.com>
    
    ---------
    
    Signed-off-by: Javier Lopez <javier@okteto.com>

---
+++ integration/actions/test_test.go
@@ -0,0 +1,137 @@
+//go:build actions
+// +build actions
+
+// Copyright 2023 The Okteto Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package actions
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+	"testing"
+
+	"github.com/okteto/okteto/integration"
+	"github.com/stretchr/testify/assert"
+)
+
+const (
+	testPath = "okteto/test"
+)
+
+func TestOktetoTestActions(t *testing.T) {
+	integration.SkipIfWindows(t)
+	namespace := integration.GetTestNamespace(t.Name())
+
+	assert.NoError(t, executeCreateNamespaceAction(namespace))
+
+	oktetoManifest, err := createOktetoTestManifest(t)
+	assert.NoError(t, err)
+
+	assert.NoError(t, executeOktetoTestAction(namespace, oktetoManifest))
+}
+
+func executeOktetoTestAction(namespace, oktetoManifest string) error {
+	actionRepo := fmt.Sprintf("%s%s.git", githubHTTPSURL, testPath)
+	actionFolder := strings.Split(testPath, "/")[1]
+	log.Printf("cloning destroy path repository: %s", actionRepo)
+	if err := integration.CloneGitRepoWithBranch(actionRepo, oktetoVersion); err != nil {
+		if err := integration.CloneGitRepo(actionRepo); err != nil {
+			return err
+		}
+		log.Printf("cloned repo %s main branch\n", actionRepo)
+	}
+	log.Printf("cloned repo %s \n", actionRepo)
+	defer integration.DeleteGitRepo(actionFolder)
+
+	builder := exec.Command("go", "build", "-o", "test", "./cmd/main.go")
+	builder.Dir = actionFolder
+	builder.Env = os.Environ()
+	o, err := builder.CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("go build: %s", string(o))
+	}
+
+	testBinary, err := filepath.Abs(filepath.Join(actionFolder, "test"))
+	if err != nil {
+		return fmt.Errorf("filepath.Abs: %s", err)
+	}
+
+	command := exec.Command(testBinary, "", namespace, "", "", "", "VAR1=Hello", "", "", "")
+	command.Env = os.Environ()
+	command.Dir = filepath.Dir(oktetoManifest)
+	o, err = command.CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("test: %s", string(o))
+	}
+
+	log.Printf("okteto test output: \n%s\n", string(o))
+
+	_, err = os.Stat(filepath.Join(filepath.Dir(oktetoManifest), "coverage.txt"))
+	if err != nil {
+		return err
+	}
+
+	_, err = os.Stat(filepath.Join(filepath.Dir(oktetoManifest), "reports/additional-coverage.txt"))
+	if err != nil {
+		return err
+	}
+
+	if !strings.Contains(string(o), "OK unit") && !strings.Contains(string(o), "OK integration") && !strings.Contains(string(o), "OK e2e") && !strings.Contains(string(o), "VAR1=Hello") {
+		return fmt.Errorf("unexpected output: %s", string(o))
+	}
+	return nil
+}
+
+func createOktetoTestManifest(t *testing.T) (string, error) {
+	manifest := `
+test:
+  unit:
+    context: .
+    image: alpine
+    commands:
+      - echo "OK unit"
+  integration:
+    context: .
+    image: alpine
+    commands:
+      - echo "OK integration"
+      - echo "OK" > coverage.txt
+      - mkdir -p reports && echo "OK" > reports/additional-coverage.txt
+    artifacts:
+      - coverage.txt
+      - reports
+  e2e:
+    context: .
+    image: alpine
+    commands:
+      - echo "OK e2e"
+  var:
+    context: .
+    image: alpine
+    commands:
+      - echo "VAR1=$VAR1"
+`
+	dir := t.TempDir()
+	log.Printf("created tempdir: %s", dir)
+	manifestPath := filepath.Join(dir, "okteto.yml")
+	manifestContent := []byte(manifest)
+	if err := os.WriteFile(manifestPath, manifestContent, 0600); err != nil {
+		return "", err
+	}
+	return manifestPath, nil
+}
