65bf651a98f788eff0b88d7b4d2beaef70eed75c
Pablo Chico de Guzman <pchico83@gmail.com>
2025-12-16 12:30:06 +0100

    Ignore discovery errors for "metrics.k8s.io/v1beta1" and "custom.metr… (#4850)
    * Ignore discovery errors for "metrics.k8s.io/v1beta1" and "custom.metrics.k8s.io/v1beta2"
    
    Signed-off-by: Pablo Chico de Guzman <pchico83@gmail.com>
    
    * Fix error condition
    
    Signed-off-by: Pablo Chico de Guzman <pchico83@gmail.com>
    
    * Add unit tests
    
    Signed-off-by: Pablo Chico de Guzman <pchico83@gmail.com>
    
    * Fix deepsource warnings
    
    Signed-off-by: Pablo Chico de Guzman <pchico83@gmail.com>
    
    ---------
    
    Signed-off-by: Pablo Chico de Guzman <pchico83@gmail.com>

--- pkg/k8s/namespaces/namespace_test.go
+++ pkg/k8s/namespaces/namespace_test.go
@@ -15,16 +15,26 @@ package namespaces
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"testing"
 
+	openapi_v2 "github.com/google/gnostic-models/openapiv2"
 	"github.com/okteto/okteto/pkg/model"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"golang.org/x/sync/semaphore"
 	appsv1 "k8s.io/api/apps/v1"
 	apiv1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/version"
+	"k8s.io/client-go/discovery"
+	dynamicfake "k8s.io/client-go/dynamic/fake"
 	"k8s.io/client-go/kubernetes/fake"
+	"k8s.io/client-go/openapi"
+	"k8s.io/client-go/rest"
 )
 
 func TestDestroySFSVolumesIfNeeded(t *testing.T) {
@@ -345,6 +355,214 @@ func TestDestroySFSVolumesIfNeeded(t *testing.T) {
 
 			assert.NoError(t, err)
 			assert.ElementsMatch(t, tt.expectedPVCs, pvcList.Items)
+		})
+	}
+}
+
+type fakeDiscovery struct {
+	errServerGroupsAndResources error
+}
+
+func (d *fakeDiscovery) ServerGroupsAndResources() ([]*metav1.APIGroup, []*metav1.APIResourceList, error) {
+	return nil, []*metav1.APIResourceList{}, d.errServerGroupsAndResources
+}
+
+// not implemented, needed for mocking the discovery client
+func (*fakeDiscovery) OpenAPISchema() (*openapi_v2.Document, error) {
+	return nil, nil
+}
+func (*fakeDiscovery) OpenAPIV3() openapi.Client {
+	return nil
+}
+func (*fakeDiscovery) RESTClient() rest.Interface {
+	return nil
+}
+func (*fakeDiscovery) ServerGroups() (*metav1.APIGroupList, error) {
+	return nil, nil
+}
+func (*fakeDiscovery) ServerPreferredNamespacedResources() ([]*metav1.APIResourceList, error) {
+	return nil, nil
+}
+func (*fakeDiscovery) ServerPreferredResources() ([]*metav1.APIResourceList, error) {
+	return nil, nil
+}
+func (*fakeDiscovery) ServerResourcesForGroupVersion(_ string) (*metav1.APIResourceList, error) {
+	return nil, nil
+}
+func (*fakeDiscovery) ServerVersion() (*version.Info, error) {
+	return nil, nil
+}
+func (*fakeDiscovery) WithLegacy() discovery.DiscoveryInterface {
+	return nil
+}
+
+type fakeK8sClient struct {
+	*fake.Clientset
+	discoveryClient discovery.DiscoveryInterface
+}
+
+func (f *fakeK8sClient) Discovery() discovery.DiscoveryInterface {
+	return f.discoveryClient
+}
+
+func Test_wander(t *testing.T) {
+	testErr := errors.New("test error")
+
+	tests := []struct {
+		name            string
+		discoveryClient discovery.DiscoveryInterface
+		expectedErr     error
+	}{
+		{
+			name: "succeeds with no errors",
+			discoveryClient: &fakeDiscovery{
+				errServerGroupsAndResources: nil,
+			},
+			expectedErr: nil,
+		},
+		{
+			name: "tolerates metrics.k8s.io error",
+			discoveryClient: &fakeDiscovery{
+				errServerGroupsAndResources: &discovery.ErrGroupDiscoveryFailed{
+					Groups: map[schema.GroupVersion]error{
+						{Group: "metrics.k8s.io", Version: "v1beta1"}: testErr,
+					},
+				},
+			},
+			expectedErr: nil,
+		},
+		{
+			name: "tolerates custom.metrics.k8s.io error",
+			discoveryClient: &fakeDiscovery{
+				errServerGroupsAndResources: &discovery.ErrGroupDiscoveryFailed{
+					Groups: map[schema.GroupVersion]error{
+						{Group: "custom.metrics.k8s.io", Version: "v1beta2"}: testErr,
+					},
+				},
+			},
+			expectedErr: nil,
+		},
+		{
+			name: "tolerates both metrics APIs errors",
+			discoveryClient: &fakeDiscovery{
+				errServerGroupsAndResources: &discovery.ErrGroupDiscoveryFailed{
+					Groups: map[schema.GroupVersion]error{
+						{Group: "metrics.k8s.io", Version: "v1beta1"}:        testErr,
+						{Group: "custom.metrics.k8s.io", Version: "v1beta2"}: testErr,
+					},
+				},
+			},
+			expectedErr: nil,
+		},
+		{
+			name: "returns error for non-metrics API",
+			discoveryClient: &fakeDiscovery{
+				errServerGroupsAndResources: &discovery.ErrGroupDiscoveryFailed{
+					Groups: map[schema.GroupVersion]error{
+						{Group: "other.k8s.io", Version: "v1"}: testErr,
+					},
+				},
+			},
+			expectedErr: &discovery.ErrGroupDiscoveryFailed{
+				Groups: map[schema.GroupVersion]error{
+					{Group: "other.k8s.io", Version: "v1"}: testErr,
+				},
+			},
+		},
+		{
+			name: "returns error for non-metrics API when metrics.k8s.io also fails",
+			discoveryClient: &fakeDiscovery{
+				errServerGroupsAndResources: &discovery.ErrGroupDiscoveryFailed{
+					Groups: map[schema.GroupVersion]error{
+						{Group: "metrics.k8s.io", Version: "v1beta1"}: testErr,
+						{Group: "other.k8s.io", Version: "v1"}:        testErr,
+					},
+				},
+			},
+			expectedErr: &discovery.ErrGroupDiscoveryFailed{
+				Groups: map[schema.GroupVersion]error{
+					{Group: "other.k8s.io", Version: "v1"}: testErr,
+				},
+			},
+		},
+		{
+			name: "returns error for non-metrics API when custom.metrics.k8s.io also fails",
+			discoveryClient: &fakeDiscovery{
+				errServerGroupsAndResources: &discovery.ErrGroupDiscoveryFailed{
+					Groups: map[schema.GroupVersion]error{
+						{Group: "custom.metrics.k8s.io", Version: "v1beta2"}: testErr,
+						{Group: "other.k8s.io", Version: "v1"}:               testErr,
+					},
+				},
+			},
+			expectedErr: &discovery.ErrGroupDiscoveryFailed{
+				Groups: map[schema.GroupVersion]error{
+					{Group: "other.k8s.io", Version: "v1"}: testErr,
+				},
+			},
+		},
+		{
+			name: "returns error for non-metrics API when both metrics APIs also fail",
+			discoveryClient: &fakeDiscovery{
+				errServerGroupsAndResources: &discovery.ErrGroupDiscoveryFailed{
+					Groups: map[schema.GroupVersion]error{
+						{Group: "metrics.k8s.io", Version: "v1beta1"}:        testErr,
+						{Group: "custom.metrics.k8s.io", Version: "v1beta2"}: testErr,
+						{Group: "other.k8s.io", Version: "v1"}:               testErr,
+					},
+				},
+			},
+			expectedErr: &discovery.ErrGroupDiscoveryFailed{
+				Groups: map[schema.GroupVersion]error{
+					{Group: "other.k8s.io", Version: "v1"}: testErr,
+				},
+			},
+		},
+		{
+			name: "returns error for multiple non-metrics APIs",
+			discoveryClient: &fakeDiscovery{
+				errServerGroupsAndResources: &discovery.ErrGroupDiscoveryFailed{
+					Groups: map[schema.GroupVersion]error{
+						{Group: "other.k8s.io", Version: "v1"}:   testErr,
+						{Group: "another.k8s.io", Version: "v1"}: testErr,
+					},
+				},
+			},
+			expectedErr: &discovery.ErrGroupDiscoveryFailed{
+				Groups: map[schema.GroupVersion]error{
+					{Group: "other.k8s.io", Version: "v1"}:   testErr,
+					{Group: "another.k8s.io", Version: "v1"}: testErr,
+				},
+			},
+		},
+		{
+			name: "returns unknown errors",
+			discoveryClient: &fakeDiscovery{
+				errServerGroupsAndResources: testErr,
+			},
+			expectedErr: testErr,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ctx := context.Background()
+			scheme := runtime.NewScheme()
+			dynamicClient := dynamicfake.NewSimpleDynamicClient(scheme)
+
+			trip := &Trip{
+				k8s:       &fakeK8sClient{discoveryClient: tt.discoveryClient},
+				dynamic:   dynamicClient,
+				namespace: "test",
+				list:      metav1.ListOptions{},
+				sem:       semaphore.NewWeighted(10),
+			}
+
+			err := trip.wander(ctx, TravelerFunc(func(_ runtime.Object) error {
+				return nil
+			}))
+
+			require.Equal(t, tt.expectedErr, err)
 		})
 	}
 }
