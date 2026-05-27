dac40a860e1b4bf7d87ef68700e747c61e84a0a4
Javier Lopez <javier@okteto.com>
2026-05-27 13:41:15 +0200

    fix(analytics): update customer_name property in PostHog to ensure consistency

--- pkg/analytics/posthog.go
+++ pkg/analytics/posthog.go
@@ -77,7 +77,7 @@ func commonPostHogProperties() posthog.Properties {
 	agent := getAgent()
 	props := posthog.Properties{
 		// Common (all PostHog sources)
-		"customer_name":     ctx.CompanyName,
+		"customer_name":   ctx.CompanyName,
 		"cluster_id":      ctx.ClusterID,
 		"cluster_version": ctx.ClusterVersion,
 		"user_id":         ctx.UserID,
